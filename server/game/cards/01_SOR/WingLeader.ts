95d8e8ccdd8f0896e46c18c40d36231176c6fa91
redwulfe <33334098+redwulfe@users.noreply.github.com>
2024-09-11 11:53:06 -0500

    changed Tie Advanced, added Wing Leader and attempted to write the Wing Leader unit test. (#38)
    I noticed that the template I was using for Wing leader called for two
    constants that it did not use and removed those. Then I copied the Tie
    Advance to implement the wing leader. I then used the Tie Advance unit
    test to create a unit test for Wing leader.
    
    ---------
    
    Co-authored-by: teadams5 <teadams5@asu.edu>

--- server/game/cards/01_SOR/WingLeader.ts
+++ server/game/cards/01_SOR/WingLeader.ts
@@ -2,24 +2,24 @@ import AbilityHelper from '../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../core/card/NonLeaderUnitCard';
 import { RelativePlayer, Trait } from '../../core/Constants';
 
-export default class TieAdvanced extends NonLeaderUnitCard {
+export default class WingLeader extends NonLeaderUnitCard {
     protected override getImplementationId() {
         return {
-            id: '4092697474',
-            internalName: 'tie-advanced',
+            id: '3443737404',
+            internalName: 'wing-leader',
         };
     }
 
     public override setupCardAbilities() {
         this.addWhenPlayedAbility({
-            title: 'Give 2 Experience tokens to a friendly Imperial unit',
+            title: 'Give 2 Experience tokens to another friendly Rebel unit',
             targetResolver: {
                 controller: RelativePlayer.Self,
-                cardCondition: (card) => card.hasSomeTrait(Trait.Imperial) && card !== this,
+                cardCondition: (card) => card.hasSomeTrait(Trait.Rebel) && card !== this,
                 immediateEffect: AbilityHelper.immediateEffects.giveExperience({ amount: 2 })
             }
         });
     }
 }
 
-TieAdvanced.implemented = true;
+WingLeader.implemented = true;
