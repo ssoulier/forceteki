8e5fd3a6c42fee885fe88decb48156e796aec500
Josh Fullmer <joshfullmer9@gmail.com>
2024-10-03 13:25:57 -0500

    feat(card): Implement Recruit (#81)
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/01_SOR/events/Recruit.ts
@@ -0,0 +1,25 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+
+export default class Recruit extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '3407775126',
+            internalName: 'recruit'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Search the top 5 cards for a unit, reveal it, and draw it',
+            immediateEffect: AbilityHelper.immediateEffects.deckSearch({
+                searchCount: 5,
+                cardCondition: (card) => card.isUnit(),
+                selectedCardsImmediateEffect: AbilityHelper.immediateEffects.drawSpecificCard()
+            })
+        });
+    }
+}
+
+Recruit.implemented = true;
+
