e977e2bdbfb2e0da062d55e415c0eb42ecf4504d
Wes Fowlks <wfowlks@users.noreply.github.com>
2024-10-03 14:19:33 -0400

    Implementing Open Fire (#83)
    Co-authored-by: Wes Fowlks <7023176+wfowlks@users.noreply.github.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/01_SOR/events/OpenFire.ts
@@ -0,0 +1,24 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { WildcardCardType } from '../../../core/Constants';
+
+export default class OpenFire extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '8148673131',
+            internalName: 'open-fire',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Deal 4 damage to a unit.',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 4 }),
+            }
+        });
+    }
+}
+
+OpenFire.implemented = true;
