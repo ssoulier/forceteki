8a7ad5f28aac1a269a42703b8abbc69146f7f58f
Valentin Jubert <val_jub@live.fr>
2024-10-03 19:02:08 +0200

    implement supercommando squad (#78)
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/units/SupercommandoSquad.ts
@@ -0,0 +1,22 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { KeywordName } from '../../../core/Constants';
+
+export default class SupercommandoSquad extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '4341703515',
+            internalName: 'supercommando-squad'
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addConstantAbility({
+            title: 'Gain sentinel while upgraded',
+            condition: (context) => context.source.isUpgraded(),
+            ongoingEffect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Sentinel)
+        });
+    }
+}
+
+SupercommandoSquad.implemented = true;
