82b68088dab16619821d9e545a660281ac3e198a
AMMayberry1 <ammayberry1@gmail.com>
2024-10-09 09:17:10 -0400

    Remove extra code from Superlaser Blast (#109)

--- server/game/cards/01_SOR/events/SuperlaserBlast.ts
+++ server/game/cards/01_SOR/events/SuperlaserBlast.ts
@@ -1,20 +1,12 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { EventCard } from '../../../core/card/EventCard';
-import { AttacksThisPhaseWatcher } from '../../../stateWatchers/AttacksThisPhaseWatcher';
-import { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
 
 export default class SuperlaserBlast extends EventCard {
-    private attacksThisPhaseWatcher: AttacksThisPhaseWatcher;
-
     protected override getImplementationId() {
         return {
             id: '1353201082',
             internalName: 'superlaser-blast',
         };
-    }
-
-    protected override setupStateWatchers(registrar: StateWatcherRegistrar) {
-        this.attacksThisPhaseWatcher = AbilityHelper.stateWatchers.attacksThisPhase(registrar, this);
     }
 
     public override setupCardAbilities() {
