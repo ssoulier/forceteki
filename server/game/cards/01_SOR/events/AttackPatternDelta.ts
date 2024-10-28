10e16a5e62b31d49b82c9e86c4e521959ef7399e
Garrett Chesnut <36425881+garrettdc2@users.noreply.github.com>
2024-10-27 20:08:02 -0500

    Implement card Attack Pattern Delta (SOR 106) (#142)
    SWUDB: https://swudb.com/card/SOR/106/attack-pattern-delta
    
    
    ![image](https://github.com/user-attachments/assets/8654d13f-9b55-456d-b1f9-4dac1c3d48b0)

---
+++ server/game/cards/01_SOR/events/AttackPatternDelta.ts
@@ -0,0 +1,55 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class AttackPatternDelta extends EventCard {
+    protected override getImplementationId () {
+        return {
+            id: '1939951561',
+            internalName: 'attack-pattern-delta',
+        };
+    }
+
+    /**
+     * Build an immediate effect that modifies power and hp for the phase equal to the passed
+     * modification amount.
+     * @param modificationAmount The amount to increase power and hp by.
+     * @returns The effect.
+     */
+    private buildModifyStatsForPhaseImmediateEffect(modificationAmount: number) {
+        return AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+            effect: AbilityHelper.ongoingEffects.modifyStats({ power: modificationAmount, hp: modificationAmount }),
+        });
+    }
+
+    public override setupCardAbilities () {
+        this.setEventAbility({
+            title: 'Give a friendly unit +3/+3 for this phase.',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                controller: RelativePlayer.Self,
+                immediateEffect: this.buildModifyStatsForPhaseImmediateEffect(3),
+            },
+            then: (firstThenContext) => ({
+                title: 'Give another friendly unit +2/+2 for this phase.',
+                targetResolver: {
+                    cardTypeFilter: WildcardCardType.Unit,
+                    cardCondition: (card) => firstThenContext.target !== card,
+                    controller: RelativePlayer.Self,
+                    immediateEffect: this.buildModifyStatsForPhaseImmediateEffect(2),
+                },
+                then: (secondThenContext) => ({
+                    title: 'Give a third friendly unit +1/+1 for this phase.',
+                    targetResolver: {
+                        cardTypeFilter: WildcardCardType.Unit,
+                        cardCondition: (card) => firstThenContext.target !== card && secondThenContext.target !== card,
+                        controller: RelativePlayer.Self,
+                        immediateEffect: this.buildModifyStatsForPhaseImmediateEffect(1),
+                    },
+                })
+            })
+        });
+    }
+}
+
+AttackPatternDelta.implemented = true;
---
+++ test/server/cards/01_SOR/events/AttackPatternDelta.spec.ts
@@ -0,0 +1,124 @@
+describe('Attack Pattern Delta', function() {
+    integration(function(contextRef) {
+        describe('Attack Pattern Delta\'s ability', function() {
+            it(
+                'should give a friendly unit +3/+3 for this phase, then give another friendly unit +2/+2 for this phase, the give a third friendly unit +1/+1 for this phase.',
+                function () {
+                    contextRef.setupTest({
+                        phase: 'action',
+                        player1: {
+                            hand: ['attack-pattern-delta'],
+                            groundArena: ['battlefield-marine', 'wampa'],
+                            spaceArena: ['red-three#unstoppable', 'alliance-xwing']
+                        },
+                        player2: {
+                            groundArena: ['atst'],
+                        }
+                    });
+
+                    const { context } = contextRef;
+
+                    // Case 1: The player can select any friendly unit card.
+                    context.player1.clickCard(context.attackPatternDelta);
+
+                    expect(context.player1).toBeAbleToSelectExactly([
+                        context.battlefieldMarine,
+                        context.wampa,
+                        context.redThree,
+                        context.allianceXwing,
+                    ]);
+
+                    // Case 2: The player selects Wampa. It gets +3/+3.
+                    context.player1.clickCard(context.wampa);
+
+                    expect(context.wampa.getPower()).toEqual(7);
+                    expect(context.wampa.getHp()).toEqual(8);
+
+                    // Case 3: The player can select any friendly unit card except Wampa.
+                    expect(context.player1).toBeAbleToSelectExactly([
+                        context.battlefieldMarine,
+                        context.redThree,
+                        context.allianceXwing,
+                    ]);
+
+                    // Case 4: The player selects Battlefield Marine. It gets +2/+2.
+                    context.player1.clickCard(context.battlefieldMarine);
+
+                    expect(context.battlefieldMarine.getPower()).toEqual(5);
+                    expect(context.battlefieldMarine.getHp()).toEqual(5);
+
+                    // Case 5: The player can select any unit friendly card except Wampa or Battlefield Marine.
+                    expect(context.player1).toBeAbleToSelectExactly([
+                        context.redThree,
+                        context.allianceXwing,
+                    ]);
+
+                    // Case 6: The player selects Red Three. It gets +1/+1.
+                    context.player1.clickCard(context.redThree);
+
+                    expect(context.redThree.getPower()).toEqual(3);
+                    expect(context.redThree.getHp()).toEqual(4);
+
+                    // Case 7: The buffs wear off at the end of the phase.
+                    context.moveToNextActionPhase();
+
+                    expect(context.wampa.getPower()).toEqual(4);
+                    expect(context.wampa.getHp()).toEqual(5);
+                    expect(context.battlefieldMarine.getPower()).toEqual(3);
+                    expect(context.battlefieldMarine.getHp()).toEqual(3);
+                    expect(context.redThree.getPower()).toEqual(2);
+                    expect(context.redThree.getHp()).toEqual(3);
+                }
+            );
+
+            it(
+                'should only give a friendly unit +3/+3 for this phase, then give another friendly unit +2/+2 for this phase when there are only 2 valid targets',
+                function () {
+                    contextRef.setupTest({
+                        phase: 'action',
+                        player1: {
+                            hand: ['attack-pattern-delta'],
+                            groundArena: ['battlefield-marine', 'wampa'],
+                        },
+                    });
+
+                    const { context } = contextRef;
+
+                    context.player1.clickCard(context.attackPatternDelta);
+                    context.player1.clickCard(context.wampa);
+
+                    // Ensure event has finished resolving by asserting active player is player2
+                    expect(context.player2).toBeActivePlayer();
+
+                    expect(context.wampa.getPower()).toEqual(7);
+                    expect(context.wampa.getHp()).toEqual(8);
+                    expect(context.battlefieldMarine.getPower()).toEqual(5);
+                    expect(context.battlefieldMarine.getHp()).toEqual(5);
+                }
+            );
+
+            it(
+                'should only give a friendly unit +3/+3 for this phase when there is only 1 valid target',
+                function () {
+                    contextRef.setupTest({
+                        phase: 'action',
+                        player1: {
+                            hand: ['attack-pattern-delta'],
+                            groundArena: ['wampa'],
+                        },
+                    });
+
+                    const { context } = contextRef;
+
+                    context.player1.clickCard(context.attackPatternDelta);
+
+                    // Ensure event has finished resolving by asserting active player is player2
+                    expect(context.player2).toBeActivePlayer();
+
+                    expect(context.wampa.getPower()).toEqual(7);
+                    expect(context.wampa.getHp()).toEqual(8);
+                }
+            );
+        });
+    });
+});
