1842e5282fea0709b3098708b33a3435b3b51e0f
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2024-11-06 12:06:44 -0600

    Implementing Gideon Hask Ruthless Loyalist (#158)
    Co-authored-by: aruizcbrands <angel.d.ruiz@cbrands.com>

---
+++ server/game/cards/01_SOR/units/GideonHaskRuthlessLoyalist.ts
@@ -0,0 +1,27 @@
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer } from '../../../core/Constants';
+import AbilityHelper from '../../../AbilityHelper';
+
+export default class GideonHaskRuthlessLoyalist extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '1664771721',
+            internalName: 'gideon-hask#ruthless-loyalist'
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addTriggeredAbility({
+            title: 'Give an Experience token to a friendly unit',
+            when: {
+                onCardDefeated: (event, context) => event.card.isUnit() && event.card.controller !== context.source.controller
+            },
+            targetResolver: {
+                controller: RelativePlayer.Self,
+                immediateEffect: AbilityHelper.immediateEffects.giveExperience()
+            }
+        });
+    }
+}
+
+GideonHaskRuthlessLoyalist.implemented = true;
---
+++ test/server/cards/01_SOR/units/GideonHaskRuthlessLoyalist.spec.ts
@@ -0,0 +1,59 @@
+describe('Gideon Hask, Ruthless Loyalist', function () {
+    integration(function (contextRef) {
+        describe('Gideon Hask\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['wampa', 'battlefield-marine', 'blizzard-assault-atat'],
+                    },
+                    player2: {
+                        hand: ['rivals-fall'],
+                        groundArena: ['gideon-hask#ruthless-loyalist', 'specforce-soldier', 'atst'],
+                    }
+                });
+            });
+
+            it('should give an Experience to a friendly unit', function () {
+                const { context } = contextRef;
+
+                // Defeating an enemy unit by battle
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.atst);
+
+                expect(context.player2).toBeAbleToSelectExactly([context.specforceSoldier, context.gideonHask, context.atst]);
+                context.player2.clickCard(context.gideonHask);
+
+                expect(context.gideonHask).toHaveExactUpgradeNames(['experience']);
+
+                // Friendly unit dies nothing happens
+                expect(context.player2).toBeActivePlayer();
+                context.player2.clickCard(context.specforceSoldier);
+                context.player2.clickCard(context.wampa);
+
+                expect(context.player1).toBeActivePlayer();
+
+                // Defeating an enemy unit using Vanquish
+                context.player1.passAction();
+                context.player2.clickCard(context.rivalsFall);
+                context.player2.clickCard(context.wampa);
+
+                expect(context.player2).toBeAbleToSelectExactly([context.gideonHask, context.atst]);
+                context.player2.clickCard(context.gideonHask);
+
+                expect(context.gideonHask).toHaveExactUpgradeNames(['experience', 'experience']);
+
+                // Ability triggers even if Gideon Hask is defeated
+                expect(context.player1).toBeActivePlayer();
+                context.setDamage(context.blizzardAssaultAtat, 5);
+                context.player1.clickCard(context.blizzardAssaultAtat);
+                context.player1.clickCard(context.gideonHask);
+                context.player1.clickPrompt('You');
+                context.player1.clickPrompt('Pass');
+                expect(context.player2).toBeAbleToSelectExactly([context.atst]);
+                context.player2.clickCard(context.atst);
+                expect(context.atst).toHaveExactUpgradeNames(['experience']);
+            });
+        });
+    });
+});
