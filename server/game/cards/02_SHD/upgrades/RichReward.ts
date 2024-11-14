8f37c2c94dc20215ab539575c4e16478bb70835b
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2024-11-14 13:50:58 -0600

    Implementing Rich Reward bounty (#188)
    ![image](https://github.com/user-attachments/assets/c77f7146-3b18-48cf-812d-574d1a00331a)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/upgrades/RichReward.ts
@@ -0,0 +1,30 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { KeywordName, RelativePlayer } from '../../../core/Constants';
+import { TargetMode } from '../../../core/Constants';
+
+export default class RichReward extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '3074091930',
+            internalName: 'rich-reward'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addGainKeywordTargetingAttached({
+            keyword: KeywordName.Bounty,
+            ability: {
+                title: 'Give an Experience token to each of up to 2 units',
+                targetResolver: {
+                    mode: TargetMode.UpTo,
+                    numCards: 2,
+                    controller: RelativePlayer.Any,
+                    immediateEffect: AbilityHelper.immediateEffects.giveExperience()
+                }
+            }
+        });
+    }
+}
+
+RichReward.implemented = true;
---
+++ test/server/cards/02_SHD/upgrades/RichReward.spec.ts
@@ -0,0 +1,84 @@
+describe('Rich Reward', function() {
+    integration(function(contextRef) {
+        describe('Rich Reward\'s Bounty ability', function() {
+            it('should give Experience token up to two units', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [
+                            { card: 'battlefield-marine', upgrades: ['rich-reward'] }, 'atst', 'yoda#old-master']
+                    },
+                    player2: {
+                        groundArena: ['wampa'],
+                        spaceArena: ['concord-dawn-interceptors']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.atst, context.yoda, context.concordDawnInterceptors]);
+                expect(context.player2).toHavePassAbilityButton();
+                expect(context.player2).toHavePrompt('Select 2 cards');
+
+                context.player2.clickCard(context.wampa);
+                context.player2.clickCard(context.concordDawnInterceptors);
+                context.player2.clickCardNonChecking(context.atst);
+                context.player2.clickPrompt('Done');
+
+                expect(context.wampa).toHaveExactUpgradeNames(['experience']);
+                expect(context.concordDawnInterceptors).toHaveExactUpgradeNames(['experience']);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should give Experience token to one unit', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [
+                            { card: 'battlefield-marine', upgrades: ['rich-reward'] }, 'atst', 'yoda#old-master']
+                    },
+                    player2: {
+                        groundArena: ['wampa'],
+                        spaceArena: ['concord-dawn-interceptors']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.atst, context.yoda, context.concordDawnInterceptors]);
+                expect(context.player2).toHavePassAbilityButton();
+                expect(context.player2).toHavePrompt('Select 2 cards');
+
+                context.player2.clickCard(context.yoda);
+                context.player2.clickPrompt('Done');
+
+                expect(context.yoda).toHaveExactUpgradeNames(['experience']);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should automatically pass if there is no other units', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['battlefield-marine']
+                    },
+                    player2: {
+                        groundArena: [{ card: 'phaseiii-dark-trooper', upgrades: ['rich-reward'] }]
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.battlefieldMarine);
+
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
