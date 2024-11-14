e1031f77882c3731dcf44831a5b76bafb901e7ae
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2024-11-14 13:51:59 -0600

    Snapshot Reflexes implementation (#199)
    ![image](https://github.com/user-attachments/assets/a2e89ab0-d862-4cc2-8199-c01001703db0)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/01_SOR/upgrades/SnapshotReflexes.ts
@@ -0,0 +1,23 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+
+export default class SnapshotReflexes extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '9985638644',
+            internalName: 'snapshot-reflexes'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addWhenPlayedAbility({
+            title: 'Attack with attached unit',
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.attack((context) => ({
+                target: context.source.parentCard
+            }))
+        });
+    }
+}
+
+SnapshotReflexes.implemented = true;
---
+++ test/server/cards/01_SOR/upgrades/SnapshotReflexes.spec.ts
@@ -0,0 +1,57 @@
+describe('Snapshot Reflexes', function() {
+    integration(function(contextRef) {
+        describe('Snapshot Reflexes\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['battlefield-marine', { card: 'wampa', exhausted: true }],
+                        hand: ['snapshot-reflexes']
+                    },
+                    player2: {
+                        groundArena: ['specforce-soldier']
+                    }
+                });
+            });
+
+            it('initiates an attack when played', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.snapshotReflexes);
+                context.player1.clickCard(context.battlefieldMarine);
+
+                expect(context.player1).toHavePassAbilityPrompt('Attack with attached unit');
+
+                context.player1.clickPrompt('Attack with attached unit');
+                context.player1.clickCard(context.specforceSoldier);
+
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['snapshot-reflexes']);
+                expect(context.battlefieldMarine.exhausted).toBe(true);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('initiates an attack but player selects not to attack', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.snapshotReflexes);
+                context.player1.clickCard(context.battlefieldMarine);
+
+                context.player1.passAction();
+
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['snapshot-reflexes']);
+                expect(context.battlefieldMarine.exhausted).toBe(false);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('does not initiates an attack when played on a unit that is already exhausted', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.snapshotReflexes);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.wampa).toHaveExactUpgradeNames(['snapshot-reflexes']);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
