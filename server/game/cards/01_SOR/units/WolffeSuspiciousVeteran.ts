828f2682fdd10fad39821c038ac0b6dc3d886394
Valentin Jubert <val_jub@live.fr>
2024-11-16 03:40:26 +0100

    wolffe (#203)

---
+++ server/game/cards/01_SOR/units/WolffeSuspiciousVeteran.ts
@@ -0,0 +1,28 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { AbilityRestriction, Duration } from '../../../core/Constants';
+
+export default class WolffeSuspiciousVeteran extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '7533529264',
+            internalName: 'wolffe#suspicious-veteran'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Bases can\'t be healed',
+            when: {
+                onCardPlayed: (event, context) => event.card === context.source,
+                onAttackDeclared: (event, context) => event.attack.attacker === context.source,
+            },
+            immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect((context) => ({
+                effect: AbilityHelper.ongoingEffects.cardCannot(AbilityRestriction.BeHealed),
+                target: [context.source.controller.base, context.source.controller.opponent.base],
+            }))
+        });
+    }
+}
+
+WolffeSuspiciousVeteran.implemented = true;
---
+++ test/server/cards/01_SOR/units/WolffeSuspiciousVeteran.spec.ts
@@ -0,0 +1,65 @@
+describe('Wolffe, Suspicious Veteran', function () {
+    integration(function (contextRef) {
+        describe('Wolffe\'s ability', function () {
+            it('should cancel heal on bases', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['wolffe#suspicious-veteran'],
+                        groundArena: ['admiral-ackbar#brilliant-strategist'],
+                        base: { card: 'echo-base', damage: 5 }
+                    },
+                    player2: {
+                        hand: ['smugglers-aid'],
+                        groundArena: ['yoda#old-master'],
+                        base: { card: 'capital-city', damage: 5 }
+                    }
+                });
+
+                const { context } = contextRef;
+
+                function reset() {
+                    context.setDamage(context.p1Base, 5);
+                    context.setDamage(context.p2Base, 5);
+                }
+
+                // play wolffe, bases can't be healed for the phase
+                context.player1.clickCard(context.wolffe);
+                expect(context.player2).toBeActivePlayer();
+
+                // nothing happen from this event
+                context.player2.clickCard(context.smugglersAid);
+                expect(context.p2Base.damage).toBe(5);
+
+                // noting happen from restore on our base
+                context.player1.clickCard(context.admiralAckbar);
+                context.player1.clickCard(context.p2Base);
+                expect(context.p1Base.damage).toBe(5);
+
+                reset();
+                context.moveToNextActionPhase();
+
+                // effect stop at the end of phase, if opponent attack before wolffe, he can heal
+                context.player1.passAction();
+                context.player2.clickCard(context.yoda);
+                context.player2.clickCard(context.p1Base);
+                expect(context.p2Base.damage).toBe(3);
+
+                // attack with wolffe, bases can't be healed for this phase
+                context.player1.clickCard(context.wolffe);
+                context.player1.clickCard(context.p2Base);
+
+                // saboteur give him a prompt too
+                context.player1.clickPrompt('Bases can\'t be healed');
+
+                reset();
+                context.player2.passAction();
+
+                // nothing happen from restore
+                context.player1.clickCard(context.admiralAckbar);
+                context.player1.clickCard(context.p2Base);
+                expect(context.p1Base.damage).toBe(5);
+            });
+        });
+    });
+});
