e9472ca8955d0a3a9149378160e6c149e04d8fea
Jason Simmons <1382389+jasimmons@users.noreply.github.com>
2024-11-19 13:28:14 -0500

    Add 02_SHD leader Finn (#195)
    This was more complicated than I anticipated. The tests might be a
    little verbose, happy to distill it down to fewer actions per test if
    that makes sense.
    
    ---------
    
    Co-authored-by: Jason Simmons <jason.simmons@valon.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/leaders/FinnThisIsARescue.ts
@@ -0,0 +1,58 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+import { RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class FinnThisIsARescue extends LeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '9596662994',
+            internalName: 'finn#this-is-a-rescue',
+        };
+    }
+
+    protected override setupLeaderSideAbilities () {
+        this.addActionAbility({
+            title: 'Defeat a friendly upgrade on a unit',
+            cost: [AbilityHelper.costs.exhaustSelf()],
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Upgrade,
+                cardCondition: (card, context) => card.controller === context.source.controller,
+                immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                    // TODO: this is a hack to store the parent card of the upgrade before it's defeated.
+                    // once last known state is implemented, we need to read the upgrade's parent card from that (same for on-attack)
+                    AbilityHelper.immediateEffects.handler((context) => ({
+                        handler: () => context.targets.upgradeParentCard = context.target.parentCard
+                    })),
+                    AbilityHelper.immediateEffects.defeat()
+                ]),
+            },
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'Give a Shield token to that unit',
+                immediateEffect: AbilityHelper.immediateEffects.giveShield({ target: ifYouDoContext.targets.upgradeParentCard }),
+            })
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities () {
+        this.addOnAttackAbility({
+            title: 'Defeat a friendly upgrade on a unit',
+            optional: true,
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Upgrade,
+                cardCondition: (card, context) => card.controller === context.source.controller,
+                immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                    AbilityHelper.immediateEffects.handler((context) => ({
+                        handler: () => context.targets.upgradeParentCard = context.target.parentCard
+                    })),
+                    AbilityHelper.immediateEffects.defeat()
+                ]),
+            },
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'Give a Shield token to that unit',
+                immediateEffect: AbilityHelper.immediateEffects.giveShield({ target: ifYouDoContext.targets.upgradeParentCard }),
+            })
+        });
+    }
+}
+
+FinnThisIsARescue.implemented = true;
---
+++ test/server/cards/02_SHD/leaders/FinnThisIsARescue.spec.ts
@@ -0,0 +1,142 @@
+describe('Finn, This is a Rescue', function () {
+    integration(function (contextRef) {
+        describe('Finn\'s undeployed ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['entrenched'],
+                        leader: { card: 'finn#this-is-a-rescue', deployed: false },
+                        groundArena: [{ card: 'battlefield-marine', upgrades: ['jedi-lightsaber'] }],
+                        resources: 4
+                    },
+                    player2: {
+                        hand: ['top-target'],
+                        groundArena: ['wampa'],
+                        resources: 5
+                    }
+                });
+            });
+
+            it('should defeat a friendly upgrade and give a shield token', function () {
+                const { context } = contextRef;
+
+                // For all scenarios, ensure an opponent's upgrade is attached. We should
+                // never be able to choose this upgrade for Finn's ability.
+                context.player1.passAction();
+                context.player2.clickCard(context.topTarget);
+                context.player2.clickCard(context.battlefieldMarine);
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['jedi-lightsaber', 'top-target']);
+
+                // Scenario 1: Defeat a friendly upgrade on a friendly unit
+                context.player1.clickCard(context.finn);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['shield', 'top-target']);
+                expect(context.jediLightsaber).toBeInZone('discard');
+                context.player2.passAction();
+                // Finn should be exhausted
+                expect(context.finn).not.toHaveAvailableActionWhenClickedBy(context.player1);
+
+                // A bit of a hack to get the shield token. Since multiple shield tokens are
+                // created, we want to ensure we get the correct one in the scenario below.
+                context.shield = context.battlefieldMarine.upgrades.filter((u) => u.name === 'Shield')[0];
+
+                // Reset
+                context.finn.exhausted = false;
+
+                // Scenario 2: Defeat a friendly upgrade on an opponent's unit
+                context.player1.clickCard(context.entrenched);
+                context.player1.clickCard(context.wampa);
+                expect(context.wampa).toHaveExactUpgradeNames(['entrenched']);
+                context.player2.passAction();
+                context.player1.clickCard(context.finn);
+                // There are now two friendly upgrades (entrenched and shield token), so we are prompted to select one
+                expect(context.player1).toBeAbleToSelectExactly([context.entrenched, context.shield]);
+                context.player1.clickCard(context.entrenched);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.wampa).toHaveExactUpgradeNames(['shield']);
+                expect(context.entrenched).toBeInZone('discard');
+
+                context.player2.passAction();
+                // Finn should be exhausted
+                expect(context.finn).not.toHaveAvailableActionWhenClickedBy(context.player1);
+            });
+        });
+
+        describe('Finn\'s deployed ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['entrenched'],
+                        leader: { card: 'finn#this-is-a-rescue', deployed: true },
+                        groundArena: [{ card: 'battlefield-marine', upgrades: ['jedi-lightsaber'] }],
+                        resources: 5
+                    },
+                    player2: {
+                        hand: ['top-target'],
+                        groundArena: ['wampa', 'atst'],
+                        resources: 5
+                    }
+                });
+            });
+
+            it('should defeat a friendly upgrade and give a shield token on attack', function () {
+                const { context } = contextRef;
+
+                const reset = (passAction = true) => {
+                    context.finn.exhausted = false;
+                    context.finn.damage = 0;
+                    context.wampa.damage = 0;
+                    if (passAction) {
+                        context.player2.passAction();
+                    }
+                };
+
+                // For all scenarios, ensure an opponent's upgrade is attached. We should
+                // never be able to choose this upgrade for Finn's ability.
+                context.player1.passAction();
+                context.player2.clickCard(context.topTarget);
+                context.player2.clickCard(context.battlefieldMarine);
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['jedi-lightsaber', 'top-target']);
+
+                // Scenario 1: Pass on defeating an upgrade on attack
+                context.player1.clickCard(context.finn);
+                context.player1.clickCard(context.wampa);
+                context.player1.passAction();
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['jedi-lightsaber', 'top-target']);
+                expect(context.wampa.damage).toBe(4);
+
+                reset();
+
+                // Scenario 2: Defeat a friendly upgrade on a friendly unit on attack
+                context.player1.clickCard(context.finn);
+                context.player1.clickCard(context.wampa);
+                context.player1.clickPrompt('Defeat a friendly upgrade on a unit');
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['shield', 'top-target']);
+                expect(context.jediLightsaber).toBeInZone('discard');
+                expect(context.wampa.damage).toBe(4);
+
+                reset();
+
+                // Scenario 3: Defeat a friendly upgrade on an opponent's unit on attack
+                // Attach a friendly upgrade to an opponent's unit
+                context.player1.clickCard(context.entrenched);
+                context.player1.clickCard(context.atst);
+                expect(context.atst).toHaveExactUpgradeNames(['entrenched']);
+
+                context.player2.passAction();
+
+                // Attack with Finn
+                context.player1.clickCard(context.finn);
+                context.player1.clickCard(context.p2Base);
+                context.player1.clickCard(context.entrenched);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.atst).toHaveExactUpgradeNames(['shield']);
+                expect(context.entrenched).toBeInZone('discard');
+            });
+        });
+    });
+});
