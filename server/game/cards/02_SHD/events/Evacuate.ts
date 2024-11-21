7c0130b1fd2b44f41cfd9507647b8a53f562fa64
brianries <briantries@gmail.com>
2024-11-21 12:57:04 -0600

    SHD# 233 Evacuate implementation (#240)
    Added evacuate logic and unit test. Made sure upgrades get
    defeated/discarded while units return to hand
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/events/Evacuate.ts
@@ -0,0 +1,26 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { RelativePlayer, Trait, WildcardCardType, WildcardZoneName } from '../../../core/Constants';
+
+export default class Evacuate extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '8261033110',
+            internalName: 'evacuate',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Return each non-leader unit to its owner\'s hand',
+            immediateEffect: AbilityHelper.immediateEffects.returnToHand((context) => {
+                // Reminder -- upgrades will get detached automatically during the leaves play handler
+                const allUnits = context.player.getUnitsInPlay().concat(context.player.opponent.getUnitsInPlay());
+                const allNonLeaderUnits = allUnits.filter((unit) => !unit.isLeaderUnit());
+                return { target: allNonLeaderUnits };
+            })
+        });
+    }
+}
+
+Evacuate.implemented = true;
---
+++ test/server/cards/02_SHD/events/Evacuate.spec.ts
@@ -0,0 +1,42 @@
+describe('Evacuate', function() {
+    integration(function(contextRef) {
+        describe('Evacuate\'s ability', function() {
+            it('should return all unit cards to hand and discard upgrades and ignore deployed leaders', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['evacuate'],
+                        groundArena: ['pyke-sentinel'],
+                        spaceArena: ['cartel-spacer'],
+                        leader: { card: 'finn#this-is-a-rescue', deployed: false },
+                    },
+                    player2: {
+                        hand: ['death-star-stormtrooper'],
+                        groundArena: ['wampa'],
+                        spaceArena: [{ card: 'imperial-interceptor', upgrades: ['academy-training'] }],
+                        leader: { card: 'grand-moff-tarkin#oversector-governor', deployed: true },
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard('evacuate');
+                expect(context.player1.hand).toHaveSize(2);
+                expect(context.player1.hand).toContain(context.pykeSentinel);
+                expect(context.player1.hand).toContain(context.cartelSpacer);
+                expect(context.player1.hand).not.toContain(context.evacuate);
+                expect(context.finn).toBeInZone('base');
+                expect(context.player1.groundArena).toHaveSize(0);
+
+                expect(context.player2.hand).toHaveSize(3);
+                expect(context.player2.hand).toContain(context.deathStarStormtrooper);
+                expect(context.player2.hand).toContain(context.wampa);
+                expect(context.player2.hand).toContain(context.imperialInterceptor);
+                expect(context.academyTraining).toBeInZone('discard');
+                expect(context.grandMoffTarkin).toBeInZone('groundArena');
+                expect(context.player2.groundArena).toHaveSize(1);
+
+                // TODO CAPTURE: test that captured units are returned to the arena and not to hand
+            });
+        });
+    });
+});
