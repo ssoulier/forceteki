86179801cad37d29a7c7b92a1ef7de72cb59709a
deluxe <131956515+c0rrupt3d-d4t4@users.noreply.github.com>
2024-11-22 20:08:11 +0100

    SHD# Kintan Intimidator card (#245)
    ![kintan-intimidator](https://github.com/user-attachments/assets/1bd0b5db-6a2f-4cda-96ff-2e121c378a5d)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/units/KintanIntimidator.ts
@@ -0,0 +1,20 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class KintanIntimidator extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '9725921907',
+            internalName: 'kintan-intimidator',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addOnAttackAbility({
+            title: 'Exhaust the defender when attacking a unit.',
+            immediateEffect: AbilityHelper.immediateEffects.exhaust((context) => ({ target: context.source.activeAttack?.target }))
+        });
+    }
+}
+
+KintanIntimidator.implemented = true;
---
+++ test/server/cards/02_SHD/units/KintanIntimidator.spec.ts
@@ -0,0 +1,36 @@
+describe('Kintan Intimidator', function () {
+    integration(function (contextRef) {
+        describe('Kintan Intimidator\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['kintan-intimidator']
+                    },
+                    player2: {
+                        groundArena: ['battlefield-marine']
+                    }
+                });
+            });
+
+            it('should exhaust the defender when attacking a unit', function () {
+                const { context } = contextRef;
+
+                // When Kintan Intimidator attacks the enemy base, nothing gets exhausted
+                context.player1.clickCard(context.kintanIntimidator);
+                context.player1.clickCard(context.p2Base);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine.exhausted).toBe(false);
+                context.player2.passAction();
+
+
+                // Select Kintan Intimidator and attack the Marine, then Marine gets exhausted
+                context.kintanIntimidator.exhausted = false;
+                context.player1.clickCard(context.kintanIntimidator);
+                context.player1.clickCard(context.battlefieldMarine);
+                expect(context.battlefieldMarine.exhausted).toBeTrue();
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
