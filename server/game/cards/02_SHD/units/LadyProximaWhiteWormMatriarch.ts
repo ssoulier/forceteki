ace84e37bb30cae94ccffe367a01e02987395261
Travis Chase <travis@travischase.me>
2024-11-30 12:04:01 -0600

    Implementing Lady Proxima, White Worm Matriarch (#287)

---
+++ server/game/cards/02_SHD/units/LadyProximaWhiteWormMatriarch.ts
@@ -0,0 +1,31 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { CardType, Trait } from '../../../core/Constants';
+
+export default class LadyProximaWhiteWormMatriarch extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '0981852103',
+            internalName: 'lady-proxima#white-worm-matriarch',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Deal 1 damage to a base.',
+            when: {
+                onCardPlayed: (event, context) =>
+                    event.card.hasSomeTrait(Trait.Underworld) &&
+                    event.card.controller === context.source.controller &&
+                    event.card !== context.source
+            },
+            optional: true,
+            targetResolver: {
+                cardTypeFilter: CardType.Base,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 }),
+            }
+        });
+    }
+}
+
+LadyProximaWhiteWormMatriarch.implemented = true;
---
+++ test/server/cards/02_SHD/units/LadyProximaWhiteWormMatriarch.spec.ts
@@ -0,0 +1,42 @@
+describe('Lady Proxima, White Worm Matriarch', function() {
+    integration(function(contextRef) {
+        describe('Lady Proxima\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['greedo#slow-on-the-draw', 'death-star-stormtrooper'],
+                        groundArena: ['lady-proxima#white-worm-matriarch']
+                    },
+                    player2: {
+                        hand: ['pyke-sentinel'],
+                    }
+                });
+            });
+
+            it('should allow dealing 1 damage to a base when playing an Underworld card', function () {
+                const { context } = contextRef;
+
+                // Player 1 plays Underworld card with Lady Proxima in play triggering ability
+                context.player1.clickCard(context.greedoSlowOnTheDraw);
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                expect(context.player1).toHavePassAbilityButton();
+
+                // Player 1 selects Player 2's base, dealing 1 damage
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(1);
+                expect(context.player2).toBeActivePlayer();
+
+                // Player 2 plays Underworld card, not triggering Lady Proxima's ability
+                context.player2.clickCard(context.pykeSentinel);
+                expect(context.p2Base.damage).toBe(1);
+                expect(context.player1).toBeActivePlayer();
+
+                // Player 1 play non-Underworld card, not triggering Lady Proxima's ability
+                context.player1.clickCard(context.deathStarStormtrooper);
+                expect(context.p2Base.damage).toBe(1);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
