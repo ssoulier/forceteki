5d1979752b93b314a606ff4616b7a2f1cde4cba4
brianries <briantries@gmail.com>
2024-12-01 12:27:04 -0600

    SOR #143 Fighters for Freedom (#272)

---
+++ server/game/cards/01_SOR/units/FightersForFreedom.ts
@@ -0,0 +1,31 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Aspect, CardType } from '../../../core/Constants';
+
+export default class FightersForFreedom extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '5907868016',
+            internalName: 'fighters-for-freedom'
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addTriggeredAbility({
+            title: 'Deal 1 damage to a base',
+            when: {
+                onCardPlayed: (event, context) =>
+                    event.card.hasSomeAspect(Aspect.Aggression) &&
+                    event.card.controller === context.source.controller &&
+                    event.card !== context.source
+            },
+            optional: true,
+            targetResolver: {
+                cardTypeFilter: CardType.Base,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 }),
+            }
+        });
+    }
+}
+
+FightersForFreedom.implemented = true;
---
+++ test/server/cards/01_SOR/units/FightersForFreedom.spec.ts
@@ -0,0 +1,36 @@
+describe('Fighters for Freedom', function() {
+    integration(function(contextRef) {
+        describe('Fighters for Freedom\'s ability', function() {
+            it('should allow dealing 1 damage to a base when playing an aggression card', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['wampa', 'wilderness-fighter'],
+                        groundArena: ['fighters-for-freedom']
+                    },
+                    player2: {
+                        hand: ['death-star-stormtrooper'],
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.wampa);
+
+                // Player1 playing aggression card triggers ability
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickCard(context.p2Base);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.p2Base.damage).toBe(1);
+
+                // Player2 playing aggression does not trigger ability
+                context.player2.clickCard(context.deathStarStormtrooper);
+                expect(context.player1).toBeActivePlayer();
+
+                // Player1 playing vigilance does not trigger ability
+                context.player1.clickCard(context.wildernessFighter);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
