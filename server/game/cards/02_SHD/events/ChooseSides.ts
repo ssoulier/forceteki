45f6751d920acd02fdfe65d8b620873258f847a6
Tommaso Madonia <tommaso@madonia.me>
2024-12-04 15:02:11 +0000

    Add Choose Sides implementation (#299)
    ![image](https://github.com/user-attachments/assets/cf313b3a-cc47-40a5-8805-6c304e693e14)

---
+++ server/game/cards/02_SHD/events/ChooseSides.ts
@@ -0,0 +1,45 @@
+import { EventCard } from '../../../core/card/EventCard';
+import AbilityHelper from '../../../AbilityHelper';
+import { RelativePlayer, WildcardCardType, WildcardZoneName } from '../../../core/Constants';
+
+export default class ChooseSides extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '2346145249',
+            internalName: 'choose-sides'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Choose a friendly non-leader unit and an enemy non-leader unit. Exchange control of those units.',
+            targetResolvers: {
+                friendlyUnit: {
+                    activePromptTitle: 'Choose a friendly non-leader unit',
+                    controller: RelativePlayer.Self,
+                    cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                    zoneFilter: WildcardZoneName.AnyArena,
+                },
+                enemyUnit: {
+                    dependsOn: 'friendlyUnit',
+                    activePromptTitle: 'Choose an enemy non-leader unit',
+                    controller: RelativePlayer.Opponent,
+                    cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                    zoneFilter: WildcardZoneName.AnyArena,
+                    immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                        AbilityHelper.immediateEffects.takeControlOfUnit((context) => ({
+                            target: context.targets.friendlyUnit,
+                            newController: context.targets.friendlyUnit.controller.opponent,
+                        })),
+                        AbilityHelper.immediateEffects.takeControlOfUnit((context) => ({
+                            target: context.targets.enemyUnit,
+                            newController: context.targets.enemyUnit.controller.opponent,
+                        })),
+                    ])
+                }
+            }
+        });
+    }
+}
+
+ChooseSides.implemented = true;
---
+++ test/server/cards/02_SHD/events/ChooseSides.spec.ts
@@ -0,0 +1,95 @@
+describe('Choose Sides', function() {
+    integration(function(contextRef) {
+        describe('Choose Sides\'s event ability', function() {
+            describe('when there are no enemy non-leader units in play', function() {
+                beforeEach(function() {
+                    contextRef.setupTest({
+                        phase: 'action',
+                        player1: {
+                            hand: ['choose-sides'],
+                            groundArena: ['echo#restored'],
+                            leader: { card: 'boba-fett#daimyo', deployed: true },
+                        },
+                        player2: {
+                            leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true },
+                        }
+                    });
+                });
+
+                it('does nothing', () => {
+                    const { context } = contextRef;
+
+                    context.player1.clickCard(context.chooseSides);
+
+                    expect(context.player2).toBeActivePlayer();
+                    expect(context.echo.controller).toBe(context.player1Object);
+                });
+            });
+
+            describe('when there are no friendly non-leader units in play', function() {
+                beforeEach(function() {
+                    contextRef.setupTest({
+                        phase: 'action',
+                        player1: {
+                            hand: ['choose-sides'],
+                            leader: { card: 'boba-fett#daimyo', deployed: true },
+                        },
+                        player2: {
+                            groundArena: ['echo#restored'],
+                            leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true },
+                        }
+                    });
+                });
+
+                it('does nothing', () => {
+                    const { context } = contextRef;
+
+                    context.player1.clickCard(context.chooseSides);
+
+                    expect(context.player2).toBeActivePlayer();
+                    expect(context.echo.controller).toBe(context.player2Object);
+                });
+            });
+
+            describe('when there are friendly and enemy non-leader units in play', function() {
+                beforeEach(function() {
+                    contextRef.setupTest({
+                        phase: 'action',
+                        player1: {
+                            hand: ['choose-sides'],
+                            groundArena: ['echo#restored'],
+                            spaceArena: ['cartel-spacer'],
+                            leader: { card: 'boba-fett#daimyo', deployed: true },
+                        },
+                        player2: {
+                            groundArena: ['echo#restored'],
+                            spaceArena: ['wing-leader'],
+                            leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true },
+                        }
+                    });
+                });
+
+                it('exchanges control of a friendly and enemy non-leader unit', () => {
+                    const { context } = contextRef;
+
+                    context.p1Echo = context.player1.findCardByName('echo#restored');
+                    context.p2Echo = context.player2.findCardByName('echo#restored');
+
+                    context.player1.clickCard(context.chooseSides);
+                    expect(context.player1).toHavePrompt('Choose a friendly non-leader unit');
+                    expect(context.player1).toBeAbleToSelectExactly([context.p1Echo, context.cartelSpacer]);
+
+                    context.player1.clickCard(context.p1Echo);
+                    expect(context.player1).toHavePrompt('Choose an enemy non-leader unit');
+                    expect(context.player1).toBeAbleToSelectExactly([context.p2Echo, context.wingLeader]);
+
+                    context.player1.clickCard(context.p2Echo);
+
+                    expect(context.player2).toBeActivePlayer();
+                    expect(context.p1Echo.controller).toBe(context.player2Object);
+                    expect(context.p2Echo.controller).toBe(context.player1Object);
+                });
+            });
+        });
+    });
+});
