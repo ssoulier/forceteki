e7c5480accd8094f97650ea6c8fc870a76ba7448
Travis Chase <travis@travischase.me>
2024-12-04 16:56:33 -0600

    Implementing Valiant Assault Ship (#300)
    ![image](https://github.com/user-attachments/assets/c18221a4-4b9a-474c-83c6-3ee8bab53712)

---
+++ server/game/cards/02_SHD/units/ValiantAssaultShip.ts
@@ -0,0 +1,27 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class ValiantAssaultShip extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '7922308768',
+            internalName: 'valiant-assault-ship'
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addOnAttackAbility({
+            title: 'If the defending player controls more resources than you, this unit gets +2/+0 for this attack',
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => context.source.controller.resources.length < context.source.controller.opponent.resources.length,
+                onTrue: AbilityHelper.immediateEffects.forThisAttackCardEffect((context) => ({
+                    target: context.source,
+                    effect: AbilityHelper.ongoingEffects.modifyStats({ power: 2, hp: 0 })
+                })),
+                onFalse: AbilityHelper.immediateEffects.noAction()
+            })
+        });
+    }
+}
+
+ValiantAssaultShip.implemented = true;
---
+++ test/server/cards/02_SHD/units/ValiantAssaultShip.spec.ts
@@ -0,0 +1,53 @@
+describe('Valiant Assault Ship', function () {
+    integration(function (contextRef) {
+        describe('Valiant Assault Ship\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        spaceArena: ['valiant-assault-ship']
+                    },
+                    player2: {
+                        spaceArena: [{ card: 'system-patrol-craft', upgrades: ['shield'] }]
+                    }
+                });
+            });
+
+            it('should give a unit +2/+0 for this attack when the opponent has more resources', function () {
+                const { context } = contextRef;
+
+                context.player1.setResourceCount(5);
+                context.player2.setResourceCount(6);
+
+                context.player1.clickCard(context.valiantAssaultShip);
+                expect(context.player1).toBeAbleToSelectExactly([context.systemPatrolCraft, context.p2Base]);
+
+                context.player1.clickCard(context.p2Base);
+                expect(context.player1).toHaveEnabledPromptButton('If the defending player controls more resources than you, this unit gets +2/+0 for this attack');
+                expect(context.player1).toHaveEnabledPromptButton('Saboteur: defeat all shields');
+                expect(context.valiantAssaultShip.exhausted).toBe(true);
+
+                context.player1.clickPrompt('If the defending player controls more resources than you, this unit gets +2/+0 for this attack');
+                expect(context.systemPatrolCraft.damage).toBe(0);
+                expect(context.p2Base.damage).toBe(5);
+            });
+
+            it('should not give a unit +2/+0 for this attack', function () {
+                const { context } = contextRef;
+
+                context.player1.setResourceCount(5);
+                context.player2.setResourceCount(5);
+
+                context.player1.clickCard(context.valiantAssaultShip);
+                expect(context.player1).toBeAbleToSelectExactly([context.systemPatrolCraft, context.p2Base]);
+
+                context.player1.clickCard(context.systemPatrolCraft);
+                expect(context.player1).toHaveEnabledPromptButton('Saboteur: defeat all shields');
+
+                context.player1.clickPrompt('Saboteur: defeat all shields');
+                expect(context.systemPatrolCraft.damage).toBe(3);
+                expect(context.systemPatrolCraft.upgrades.length).toBe(0);
+            });
+        });
+    });
+});
