14294591ecbd0b8b5df94ae61cafdffa1a6c863d
Travis Chase <travis@travischase.me>
2024-12-05 17:09:15 -0500

    Implementing Guarding the Way (#303)
    ![image](https://github.com/user-attachments/assets/a5d8c002-96ff-4dd2-b106-b69418b83a24)
    
    I didn't see in the tests for a way to check for Sentinel other than
    just attacking so that is the direction I went.

---
+++ server/game/cards/03_TWI/events/GuardingTheWay.ts
@@ -0,0 +1,35 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { KeywordName, WildcardCardType } from '../../../core/Constants';
+
+export default class GuardingTheWay extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '2359136621',
+            internalName: 'guarding-the-way',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Give Sentinel for the phase. If you have the initiative, also give that unit +2/+2 for this phase.',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                    AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                        effect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Sentinel)
+                    }),
+                    AbilityHelper.immediateEffects.conditional({
+                        condition: (context) => context.source.controller.hasInitiative(),
+                        onTrue: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                            effect: AbilityHelper.ongoingEffects.modifyStats({ power: 2, hp: 2 })
+                        }),
+                        onFalse: AbilityHelper.immediateEffects.noAction()
+                    })
+                ])
+            }
+        });
+    }
+}
+
+GuardingTheWay.implemented = true;
---
+++ test/server/cards/03_TWI/events/GuardingTheWay.spec.ts
@@ -0,0 +1,69 @@
+describe('Guarding the Way', function () {
+    integration(function (contextRef) {
+        describe('Guarding the Way\'s ability', function () {
+            it('should give a unit Sentinel and +2/+2 for the phase when the controller has initiative', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['guarding-the-way'],
+                        groundArena: ['luke-skywalker#jedi-knight']
+                    },
+                    player2: {
+                        groundArena: ['chewbacca#pykesbane']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.guardingTheWay);
+                expect(context.player1).toBeAbleToSelectExactly([context.lukeSkywalkerJediKnight, context.chewbaccaPykesbane]);
+
+                context.player1.clickCard(context.lukeSkywalkerJediKnight);
+                expect(context.lukeSkywalkerJediKnight.getPower()).toBe(8);
+                expect(context.lukeSkywalkerJediKnight.getHp()).toBe(9);
+                expect(context.player2).toBeActivePlayer();
+
+                // Attack verifying Sentinel and +2/+2 were applied
+                context.player2.clickCard(context.chewbaccaPykesbane);
+                expect(context.chewbaccaPykesbane.damage).toBe(8);
+                expect(context.lukeSkywalkerJediKnight.damage).toBe(4);
+
+                context.moveToNextActionPhase();
+                expect(context.lukeSkywalkerJediKnight.getPower()).toBe(6);
+                expect(context.lukeSkywalkerJediKnight.getHp()).toBe(7);
+            });
+
+            it('should give a unit Sentinel for the phase when the controller does not have initiative', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['pyke-sentinel'],
+                        groundArena: ['chewbacca#pykesbane'],
+                    },
+                    player2: {
+                        hand: ['guarding-the-way'],
+                        groundArena: ['luke-skywalker#jedi-knight']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.pykeSentinel);
+                expect(context.player2).toBeActivePlayer();
+
+                context.player2.clickCard(context.guardingTheWay);
+                expect(context.player2).toBeAbleToSelectExactly([context.lukeSkywalkerJediKnight, context.chewbaccaPykesbane, context.pykeSentinel]);
+
+                context.player2.clickCard(context.lukeSkywalkerJediKnight);
+                expect(context.lukeSkywalkerJediKnight.getPower()).toBe(6);
+                expect(context.lukeSkywalkerJediKnight.getHp()).toBe(7);
+                expect(context.player1).toBeActivePlayer();
+
+                // Attack verifying Sentinel was applied
+                context.player1.clickCard(context.chewbaccaPykesbane);
+                expect(context.lukeSkywalkerJediKnight.damage).toBe(4);
+                expect(context.chewbaccaPykesbane.damage).toBe(6);
+            });
+        });
+    });
+});
