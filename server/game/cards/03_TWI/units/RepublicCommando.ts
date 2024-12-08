9726fc7df4ab766e2c627b29e8825dc1102901f6
Travis Chase <travis@travischase.me>
2024-12-08 11:58:44 -0500

    Implementing 332nd Stalwart and Republic Commando (#308)
    ![image](https://github.com/user-attachments/assets/27a0d14e-f806-4c2d-9aea-06bd6d56f6a8)
    
    ![image](https://github.com/user-attachments/assets/c5f80b52-97e9-43c1-a098-92b5695016e5)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/units/RepublicCommando.ts
@@ -0,0 +1,22 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { AbilityType, KeywordName } from '../../../core/Constants';
+
+export default class RepublicCommando extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '8187818742',
+            internalName: 'republic-commando',
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addCoordinateAbility({
+            type: AbilityType.Constant,
+            title: 'Gain Saboteur',
+            ongoingEffect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Saboteur)
+        });
+    }
+}
+
+RepublicCommando.implemented = true;
