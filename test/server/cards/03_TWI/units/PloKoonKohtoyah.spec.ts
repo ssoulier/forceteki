4c3dbc1965d4cd78dfa9d293f32179266193ae58
Stephane Soulier <stephane.soulier@alan.com>
2024-12-08 18:23:14 +0100

    update plo koon test

--- test/server/cards/03_TWI/units/PloKoonKohtoyah.spec.ts
+++ test/server/cards/03_TWI/units/PloKoonKohtoyah.spec.ts
@@ -1,15 +1,15 @@
 describe('Plo Koon Kohtoyah', function () {
     integration(function (contextRef) {
         describe('Plo Koon Kohtoyah\'s ability', function () {
-            it('should ambush attack for 6', function () {
+            it('should ambush attack for 6 and defend for 3', function () {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
                         hand: ['plo-koon#kohtoyah'],
-                        groundArena: ['obiwan-kenobi#following-fate', 'luke-skywalker#jedi-knight'],
+                        groundArena: ['specforce-soldier', 'luke-skywalker#jedi-knight'],
                     },
                     player2: {
-                        groundArena: ['consular-security-force'],
+                        groundArena: ['consular-security-force', 'wampa'],
                     }
                 });
 
@@ -17,27 +17,34 @@ describe('Plo Koon Kohtoyah', function () {
 
                 context.player1.clickCard(context.ploKoon);
                 context.player1.clickPrompt('Ambush');
+                context.player1.clickCard(context.consularSecurityForce);
                 expect(context.consularSecurityForce.damage).toBe(6);
                 expect(context.ploKoon.damage).toBe(3);
                 expect(context.ploKoon.exhausted).toBeTrue();
+
+                context.player2.clickCard(context.wampa);
+                context.player2.clickCard(context.ploKoon);
+                expect(context.wampa.damage).toBe(3);
             });
 
-            it('should defend back for 3 damage', function () {
+            it('should not active coordinate', function () {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
-                        groundArena: ['obiwan-kenobi#following-fate'],
+                        hand: ['plo-koon#kohtoyah'],
+                        groundArena: ['specforce-soldier'],
                     },
                     player2: {
-                        groundArena: ['wampa', 'luke-skywalker#jedi-knight', 'plo-koon#kohtoyah'],
+                        groundArena: ['wampa'],
                     }
                 });
 
                 const { context } = contextRef;
 
-                context.player1.clickCard(context.obiwanKenobi);
                 context.player1.clickCard(context.ploKoon);
-                expect(context.obiwanKenobi.damage).toBe(3);
+                context.player1.clickPrompt('Ambush');
+                expect(context.wampa.damage).toBe(3);
+                expect(context.ploKoon.damage).toBe(4);
             });
         });
     });
