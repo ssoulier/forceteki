2f50222e530fcd5656bf03ef562ac4c4f38c4116
Stephane Soulier <stephane.soulier@alan.eu>
2024-12-11 20:07:44 +0100

    TWI hevy staunch martyr (#321)
    Adding [Hevy](https://swudb.com/card/TWI/164/hevy).
    
    <img width="422" alt="Screenshot 2024-12-11 at 16 01 35"
    src="https://github.com/user-attachments/assets/c7f55f18-92c0-47b9-9c2a-367a1d4d5f06"
    />
    
    
    I lost time understanding that I should not use targetResolver. I am
    still learning :)
    
    ---------
    
    Co-authored-by: Stephane Soulier <stephane.soulier@alan.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/units/HevyStaunchMartyr.ts
@@ -0,0 +1,30 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { AbilityType, KeywordName, ZoneName } from '../../../core/Constants';
+
+export default class HevyStaunchMartyr extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '0683052393',
+            internalName: 'hevy#staunch-martyr'
+        };
+    }
+
+    protected override setupCardAbilities() {
+        this.addCoordinateAbility({
+            type: AbilityType.Constant,
+            title: 'Gain Raid 2',
+            ongoingEffect: AbilityHelper.ongoingEffects.gainKeyword({ keyword: KeywordName.Raid, amount: 2 }),
+        });
+
+        this.addWhenDefeatedAbility({
+            title: 'Deal 1 damage to each enemy ground unit.',
+            immediateEffect: AbilityHelper.immediateEffects.damage((context) => ({
+                amount: 1,
+                target: context.source.controller.opponent.getArenaUnits({ arena: ZoneName.GroundArena })
+            }))
+        });
+    }
+}
+
+HevyStaunchMartyr.implemented = true;
