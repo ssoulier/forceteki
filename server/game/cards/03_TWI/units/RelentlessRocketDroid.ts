21c7776fe9f26b386a133c8bdd1bce1c78308458
Stephane Soulier <stephane.soulier@alan.eu>
2024-12-13 00:43:17 +0100

    add relentless rocket droid (#327)
    Adding
    [relentless-rocket-droid](https://www.swudb.com/card/TWI/163/relentless-rocket-droid)
    
    <img width="416" alt="Screenshot 2024-12-12 at 08 15 51"
    src="https://github.com/user-attachments/assets/979477a9-eb7e-486d-a27b-0b6d4ffbf6a8"
    />
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/units/RelentlessRocketDroid.ts
@@ -0,0 +1,22 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Trait } from '../../../core/Constants';
+
+export default class RelentlessRocketDroid extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '1368135704',
+            internalName: 'relentless-rocket-droid'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addConstantAbility({
+            title: 'While you control another Trooper unit, this unit gets +2/+0.',
+            condition: (context) => context.source.controller.getOtherUnitsInPlayWithTrait(context.source, Trait.Trooper).length > 0,
+            ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: 2, hp: 0 })
+        });
+    }
+}
+
+RelentlessRocketDroid.implemented = true;
