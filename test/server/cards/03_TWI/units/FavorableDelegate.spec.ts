094d7a35365b9da6dd0c9882f945a60794a704ce
Hugo Humez <52443431+HugoHMZ@users.noreply.github.com>
2024-12-13 15:39:48 +0100

    [+] FavorableDelegate: Card Implementation Draft (#328)
    ![image](https://github.com/user-attachments/assets/7c975b5b-a12e-4764-8311-98ad42ea60ee)
    
    ---------
    
    Co-authored-by: HugoHMZ <hugo.humez.p@gmail.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ test/server/cards/03_TWI/units/FavorableDelegate.spec.ts
@@ -0,0 +1,50 @@
+describe('Favorable Delegate\'s', function () {
+    integration(function (contextRef) {
+        describe('abilities', function () {
+            it('should draw a card when played and discard when defeated', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['favorable-delegate']
+                    },
+                    player2: {
+                        leader: { card: 'mace-windu#vaapad-form-master', deployed: true },
+                    }
+                });
+                const { context } = contextRef;
+
+                // Check card drawn when played
+                context.player1.clickCard(context.favorableDelegate);
+                expect(context.player1.handSize).toBe(1);
+                expect(context.player2).toBeActivePlayer();
+
+                // Check card discarded when defeated
+                context.player2.clickCard(context.maceWindu);
+                context.player2.clickCard(context.favorableDelegate);
+                expect(context.player1.handSize).toBe(0);
+                expect(context.player1).toBeActivePlayer();
+            });
+
+            it('should discard nothing if empty hand', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['favorable-delegate']
+                    },
+                    player2: {
+                        leader: { card: 'mace-windu#vaapad-form-master', deployed: true },
+                        hasInitiative: true,
+                    }
+                });
+                const { context } = contextRef;
+                context.player1.setHand([]);
+
+                // Attacking Delegate to trigger the When defeated ability
+                context.player2.clickCard(context.maceWindu);
+                context.player2.clickCard(context.favorableDelegate);
+                expect(context.player1.handSize).toBe(0);
+                expect(context.player1).toBeActivePlayer();
+            });
+        });
+    });
+});
