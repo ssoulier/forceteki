764ab97bc91f42783dbb9dcf96222d9803125b76
Stephane Soulier <stephane.soulier@alan.eu>
2024-12-13 15:42:44 +0100

    add Independent Senator (#330)
    [Independent
    Senator](https://www.swudb.com/card/TWI/206/independent-senator)
    
    <img width="417" alt="Screenshot 2024-12-12 at 17 11 06"
    src="https://github.com/user-attachments/assets/d5981284-0858-48d4-b202-7828b4f3a444"
    />
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ test/server/cards/03_TWI/units/IndependentSenator.spec.ts
@@ -0,0 +1,30 @@
+describe('Independent Senator', function () {
+    integration(function (contextRef) {
+        it('Independent Senator\'s should exhaust a unit with 4 or less power', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    groundArena: ['independent-senator']
+                },
+                player2: {
+                    groundArena: ['wampa', 'atst'],
+                    spaceArena: ['gladiator-star-destroyer', 'green-squadron-awing']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.independentSenator);
+            context.player1.clickPrompt('Exhaust a unit with 4 or less power.');
+
+            // can choose any units
+            expect(context.player1).toBeAbleToSelectExactly([context.greenSquadronAwing, context.wampa, context.independentSenator]);
+
+            // Exhaust green squadron A wing
+            context.player1.clickCard(context.greenSquadronAwing);
+            expect(context.greenSquadronAwing.exhausted).toBeTrue();
+            expect(context.independentSenator.exhausted).toBeTrue();
+            expect(context.player1.exhaustedResourceCount).toBe(2);
+        });
+    });
+});
