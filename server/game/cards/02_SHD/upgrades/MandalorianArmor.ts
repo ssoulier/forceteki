46a8a81655b4c2e711cafc8a4fe63b6e75acfcad
Twet1 <124315575+Twet1@users.noreply.github.com>
2024-12-20 03:09:24 +0000

    Mando Armor SHD (#363)
    ![image](https://github.com/user-attachments/assets/0dee0e5a-0009-49e8-901a-9c13fdaee790)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/upgrades/MandalorianArmor.ts
@@ -0,0 +1,27 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { Trait } from '../../../core/Constants';
+import { Card } from '../../../core/card/Card';
+
+export default class MandalorianArmor extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '3514010297',
+            internalName: 'mandalorian-armor'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setAttachCondition((card: Card) => !card.hasSomeTrait(Trait.Vehicle));
+
+        this.addWhenPlayedAbility({
+            title: 'Give a Shield token to attached unit.',
+            immediateEffect: AbilityHelper.immediateEffects.conditional((context) => ({ condition: context.source.parentCard?.hasSomeTrait(Trait.Mandalorian),
+                onTrue: AbilityHelper.immediateEffects.giveShield({ target: context.source.parentCard }),
+                onFalse: AbilityHelper.immediateEffects.noAction() }),
+            )
+        });
+    }
+}
+
+MandalorianArmor.implemented = true;
---
+++ test/server/cards/02_SHD/upgrades/MandalorianArmor.spec.ts
@@ -0,0 +1,36 @@
+describe('Mandalorian Armor', function() {
+    integration(function(contextRef) {
+        it('Mandalorian Armor\'s attached ability should give a Shield token to attached card if it has Mandalorian trait', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    groundArena: ['ketsu-onyo#old-friend', 'the-mandalorian#wherever-i-go-he-goes', 'battlefield-marine'],
+                    spaceArena: ['clan-saxon-gauntlet'],
+                    hand: ['mandalorian-armor']
+                },
+                player2: {
+                    groundArena: ['pyke-sentinel'],
+                    hand: ['vanquish']
+                }
+            });
+
+            const { context } = contextRef;
+            // test1: mando armor played on a mando trait unit.
+            context.player1.clickCard(context.mandalorianArmor);
+            expect(context.player1).toBeAbleToSelectExactly([context.ketsuOnyo, context.theMandalorian, context.battlefieldMarine, context.pykeSentinel]);
+            context.player1.clickCard(context.theMandalorian);
+            expect(context.theMandalorian).toHaveExactUpgradeNames(['shield', 'mandalorian-armor']);
+
+            // remove unit and sent upgrade back to hand
+            context.player2.clickCard(context.vanquish);
+            context.player2.clickCard(context.theMandalorian);
+            context.player1.moveCard(context.mandalorianArmor, 'hand');
+
+            // test2: play mando armor on opponents unit, no shield should be given as not a mandalorian
+            context.player1.clickCard(context.mandalorianArmor);
+            expect(context.player1).toBeAbleToSelectExactly([context.ketsuOnyo, context.battlefieldMarine, context.pykeSentinel]);
+            context.player1.clickCard(context.pykeSentinel);
+            expect(context.pykeSentinel).toHaveExactUpgradeNames(['mandalorian-armor']);
+        });
+    });
+});
