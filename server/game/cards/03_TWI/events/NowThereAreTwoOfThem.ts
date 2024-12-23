60de5ee391b5d67868d6386da699a24e4158e424
Stephane Soulier <stephane.soulier@alan.eu>
2024-12-23 01:27:17 +0100

    Now there are two of them (#372)
    [Now there are 2 of
    them](https://swudb.com/card/TWI/225/now-there-are-two-of-them)
    
    <img width="359" alt="Screenshot 2024-12-21 at 09 55 53"
    src="https://github.com/user-attachments/assets/76cd800b-2c01-46b9-ae09-ec337aaedc52"
    />
    
    ---------
    
    Co-authored-by: Stephane Soulier <stephane.soulier@alan.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/events/NowThereAreTwoOfThem.ts
@@ -0,0 +1,36 @@
+import { EventCard } from '../../../core/card/EventCard';
+import AbilityHelper from '../../../AbilityHelper';
+import { CardType, Trait, ZoneName } from '../../../core/Constants';
+import { CostAdjustType } from '../../../core/cost/CostAdjuster';
+
+export default class NowThereAreTwoOfThem extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '6849037019',
+            internalName: 'now-there-are-two-of-them'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'If you control exactly one unit, play a non-Vehicle unit from your hand that shares a Trait with the unit you control. It costs 5 less.',
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => context.source.controller.getUnitsInPlay().length === 1,
+                onFalse: AbilityHelper.immediateEffects.noAction(),
+                onTrue: AbilityHelper.immediateEffects.selectCard({
+                    activePromptTitle: 'Play a non-Vehicle unit from your hand that shared a Trait with the unit you control. It costs 5 less.',
+                    cardTypeFilter: CardType.BasicUnit,
+                    zoneFilter: ZoneName.Hand,
+                    cardCondition: (card, context) =>
+                        !card.hasSomeTrait(Trait.Vehicle) &&
+                        Array.from(context.source.controller.getUnitsInPlay()[0].traits).some((trait) => card.hasSomeTrait(trait)),
+                    innerSystem: AbilityHelper.immediateEffects.playCardFromHand({
+                        adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 5 }
+                    })
+                }),
+            })
+        });
+    }
+}
+
+NowThereAreTwoOfThem.implemented = true;
---
+++ test/server/cards/03_TWI/events/NowThereAreTwoOfThem.spec.ts
@@ -0,0 +1,116 @@
+describe('Now There are Two of Them', function() {
+    integration(function(contextRef) {
+        describe('Now There are Two of Them\'s ability -', function() {
+            it('should allow you to play a non-Vehicle unit from your hand that shares a Trait with the unit you control for 5 less, if you have only one unit in play', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: [
+                            'now-there-are-two-of-them',
+                            'syndicate-lackeys', // Selectable, only underworld Trait
+                            'toro-calican#ambitious-upstart', // Selectable, only bounty hunter Trait
+                            'cad-bane#hostage-taker', // The one selected and played
+                            'plo-koon#kohtoyah', // Does not share a trait with Greedo
+                            'fetts-firespray#pursuing-the-bounty', // Vehicle can't be selected
+                            'chewbacca#pykesbane' // Too expensive to be played
+                        ],
+                        groundArena: ['greedo#slow-on-the-draw'],
+                        base: { card: 'administrators-tower', damage: 0 },
+                        leader: 'qira#i-alone-survived',
+                        resources: 5
+                    },
+                    player2: {
+                        hand: ['underworld-thug'], // Opponent's card can't be selected
+                        groundArena: ['r2d2#full-of-solutions'], // Shares Republic trait with Plo-koon but should not m
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.nowThereAreTwoOfThem);
+                expect(context.player1.exhaustedResourceCount).toBe(3);
+                expect(context.player1).toBeAbleToSelectExactly([context.toroCalican, context.cadBane, context.syndicateLackeys]);
+                context.player1.clickCard(context.cadBane);
+                expect(context.cadBane).toBeInZone('groundArena');
+                expect(context.player1.exhaustedResourceCount).toBe(5);
+            });
+
+            it('should not allow you to play a card if none share a trait with your unit', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: [
+                            'now-there-are-two-of-them',
+                            'plo-koon#kohtoyah', // Does not share a trait with Greedo
+                            'fetts-firespray#pursuing-the-bounty', // Vehicle can't be selected
+                            'chewbacca#pykesbane' // Too expensive to be played
+                        ],
+                        groundArena: ['greedo#slow-on-the-draw'],
+                        base: { card: 'administrators-tower', damage: 0 },
+                        leader: 'qira#i-alone-survived',
+                        resources: 5
+                    },
+                    player2: {
+                        hand: ['underworld-thug'], // Opponent's card can't be selected
+                        groundArena: ['r2d2#full-of-solutions'], // Shares Republic trait with Plo-koon but should not m
+                    }
+                });
+
+                const { context } = contextRef;
+
+                // There is nothing you can play. Player 2 becomes the active player
+                context.player1.clickCard(context.nowThereAreTwoOfThem);
+                expect(context.player1.exhaustedResourceCount).toBe(3);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not allow you to play a card if you have more than 1 unit in play or 0 unit', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: [
+                            'now-there-are-two-of-them',
+                            'zuckuss#bounty-hunter-for-hire',
+                            'plo-koon#kohtoyah',
+                            'chewbacca#pykesbane'
+                        ],
+                        groundArena: ['greedo#slow-on-the-draw', 'cad-bane#hostage-taker'],
+                        base: { card: 'administrators-tower', damage: 0 },
+                        leader: 'qira#i-alone-survived',
+                    }
+                });
+
+                const { context } = contextRef;
+
+                // You can't play a card if you have more than one unit in play.
+                context.player1.clickCard(context.nowThereAreTwoOfThem);
+                expect(context.player1.exhaustedResourceCount).toBe(3);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not allow you to play a card if you have 0 unit in play', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: [
+                            'now-there-are-two-of-them',
+                            'zuckuss#bounty-hunter-for-hire',
+                            'plo-koon#kohtoyah',
+                            'chewbacca#pykesbane'
+                        ],
+                        groundArena: [],
+                        base: { card: 'administrators-tower', damage: 0 },
+                        leader: 'qira#i-alone-survived',
+                    }
+                });
+
+                const { context } = contextRef;
+
+                // You can't play a card if you have 0 unit in play.
+                context.player1.clickCard(context.nowThereAreTwoOfThem);
+                expect(context.player1.exhaustedResourceCount).toBe(3);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
