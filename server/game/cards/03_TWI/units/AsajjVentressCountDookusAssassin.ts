56a45ec6746ed3ecfca08bbc0127a080ab9ec566
Stephane Soulier <stephane.soulier@alan.eu>
2024-12-27 04:44:03 +0100

    add asajj ventress count dooku assassin (#390)
    [Asajj Ventress](https://swudb.com/card/twi/134/asajj-ventress)
    
    <img width="349" alt="Screenshot 2024-12-24 at 12 50 59"
    src="https://github.com/user-attachments/assets/d24f3428-0f2f-4da9-8d0e-1117e62806c0"
    />
    
    ---------
    
    Co-authored-by: Stephane Soulier <stephane.soulier@alan.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/units/AsajjVentressCountDookusAssassin.ts
@@ -0,0 +1,40 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Trait } from '../../../core/Constants';
+import { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import { AttacksThisPhaseWatcher } from '../../../stateWatchers/AttacksThisPhaseWatcher';
+
+export default class AsajjVentressCountDookusAssassin extends NonLeaderUnitCard {
+    private attacksThisPhaseWatcher: AttacksThisPhaseWatcher;
+
+    protected override getImplementationId () {
+        return {
+            id: '3556557330',
+            internalName: 'asajj-ventress#count-dookus-assassin',
+        };
+    }
+
+    protected override setupStateWatchers(registrar: StateWatcherRegistrar) {
+        this.attacksThisPhaseWatcher = AbilityHelper.stateWatchers.attacksThisPhase(registrar, this);
+    }
+
+    public override setupCardAbilities () {
+        this.addOnAttackAbility({
+            title: 'If you\'ve attacked with another Separatist unit this phase, this unit gets +3/+0 for this phase.',
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => this.attacksThisPhaseWatcher.someUnitAttackedControlledByPlayer({
+                    controller: context.source.controller,
+                    filter: (attackEvent) =>
+                        attackEvent.attacker.hasSomeTrait(Trait.Separatist) &&
+                        (attackEvent.attacker !== context.source || attackEvent.attackerInPlayId !== context.source.inPlayId)
+                }),
+                onTrue: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: AbilityHelper.ongoingEffects.modifyStats({ power: 3, hp: 0 }),
+                }),
+                onFalse: AbilityHelper.immediateEffects.noAction()
+            })
+        });
+    }
+}
+
+AsajjVentressCountDookusAssassin.implemented = true;
---
+++ test/server/cards/03_TWI/units/AsajjVentressCountDookusAssassin.spec.ts
@@ -0,0 +1,73 @@
+describe('Asajj Ventress Count Doooku\'s Assassin', function() {
+    integration(function(contextRef) {
+        it('Asajj Ventress Count Doooku\'s Assassin\'s ability on attack should get +3/0 if a separatist unit has attacked this phase', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    hand: ['keep-fighting'],
+                    groundArena: ['asajj-ventress#count-dookus-assassin', 'relentless-rocket-droid', 'shaak-ti#unity-wins-wars']
+                },
+                player2: {
+                    hand: ['waylay'],
+                    groundArena: ['consular-security-force', 'separatist-commando']
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Attack with a Separatist unit
+            context.player1.clickCard(context.relentlessRocketDroid);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(3);
+            context.player2.passAction();
+
+            // When Attack Asajj Ventress gets +3/+0 for the phase
+            context.player1.clickCard(context.asajjVentress);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(8);
+
+            // Consular attack back on Asajj Ventress
+            context.player2.clickCard(context.consularSecurityForce);
+            context.player2.clickCard(context.asajjVentress);
+            expect(context.consularSecurityForce.damage).toBe(5);
+            expect(context.asajjVentress.damage).toBe(3);
+
+            // Asajj Ventress should not get +3/+0 when the next-phase starts
+            context.nextPhase();
+            expect(context.asajjVentress.getPower()).toBe(2);
+            expect(context.asajjVentress.remainingHp).toBe(1);
+
+            // Even if Asaaj Ventress is Separatist, she should not get +3/+0 when she attacks
+            context.nextPhase();
+            context.player1.clickCard(context.asajjVentress);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(10);
+
+            // But if a new copy of Asajj Ventress is played, she should get +3/+0
+            context.player2.clickCard(context.waylay);
+            context.player2.clickCard(context.asajjVentress);
+            context.player1.clickCard(context.asajjVentress);
+            context.player2.passAction();
+            context.player1.clickCard(context.keepFighting);
+            context.player1.clickCard(context.asajjVentress);
+            context.player2.passAction();
+            context.player1.clickCard(context.asajjVentress);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(15);
+
+            // Shaak Ti is not sepratist so Asajj Ventress should not get +3/+0
+            // Even if an opponent separatist unit has attacked it should not get +3/+0
+            context.moveToNextActionPhase();
+            context.player1.clickCard(context.shaakTi);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(18);
+
+            context.player2.clickCard(context.separatistCommando);
+            context.player2.clickCard(context.p1Base);
+            context.player1.clickCard(context.asajjVentress);
+            context.player1.clickCard(context.p2Base);
+            expect(context.asajjVentress.getPower()).toBe(2);
+            expect(context.p2Base.damage).toBe(20);
+        });
+    });
+});
