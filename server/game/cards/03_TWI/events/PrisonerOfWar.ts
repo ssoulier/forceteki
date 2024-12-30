9287fa6531d83e9e3cf35c8d85a905c1637e4876
Travis Chase <travis@travischase.me>
2024-12-30 09:47:01 -0600

    Implementing Prisoner of War (#400)
    ![image](https://github.com/user-attachments/assets/ebcd9409-e441-4a5f-969f-67f880bfda89)
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/events/PrisonerOfWar.ts
@@ -0,0 +1,46 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { RelativePlayer, Trait, WildcardCardType, WildcardZoneName } from '../../../core/Constants';
+
+export default class PrisonerOfWar extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '3799780905',
+            internalName: 'prisoner-of-war',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'A friendly unit captures an enemy non-leader, non-Vehicle unit. If the enemy unit costs less than the friendly unit, create 2 Battle Droid tokens.',
+            targetResolvers: {
+                friendlyUnit: {
+                    controller: RelativePlayer.Self,
+                    cardTypeFilter: WildcardCardType.Unit,
+                    zoneFilter: WildcardZoneName.AnyArena
+                },
+                captureUnit: {
+                    dependsOn: 'friendlyUnit',
+                    controller: RelativePlayer.Opponent,
+                    zoneFilter: WildcardZoneName.AnyArena,
+                    cardCondition: (card, context) => card.isNonLeaderUnit() && !card.hasSomeTrait(Trait.Vehicle),
+                    immediateEffect: AbilityHelper.immediateEffects.sequential([
+                        AbilityHelper.immediateEffects.capture((context) => ({
+                            captor: context.targets.friendlyUnit
+                        })),
+                        AbilityHelper.immediateEffects.conditional({
+                            condition: (context) => context.targets.captureUnit.cost < context.targets.friendlyUnit.cost,
+                            onTrue: AbilityHelper.immediateEffects.createBattleDroid((context) => ({
+                                target: context.targets.friendlyUnit.controller,
+                                amount: 2
+                            })),
+                            onFalse: AbilityHelper.immediateEffects.noAction(),
+                        })
+                    ])
+                }
+            }
+        });
+    }
+}
+
+PrisonerOfWar.implemented = true;
---
+++ test/server/cards/03_TWI/events/PrisonerOfWar.spec.ts
@@ -0,0 +1,59 @@
+describe('Prisoner of War', function() {
+    integration(function(contextRef) {
+        describe('Prisoner of War\'s event ability', function() {
+            beforeEach(function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['prisoner-of-war'],
+                        groundArena: ['ezra-bridger#resourceful-troublemaker'],
+                        spaceArena: ['millennium-falcon#piece-of-junk'],
+                    },
+                    player2: {
+                        groundArena: ['pyke-sentinel', 'gideon-hask#ruthless-loyalist'],
+                        spaceArena: ['inferno-four#unforgetting'],
+                        leader: { card: 'director-krennic#aspiring-to-authority', deployed: true }
+                    }
+                });
+            });
+
+            it('should allow a friendly unit to capture an enemy non-leader, non-vehicle unit generating 2 battle droid tokens because the captured unit costs less', () => {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.prisonerOfWar);
+                expect(context.player1).toBeAbleToSelectExactly([context.ezraBridger, context.millenniumFalcon]);
+
+                context.player1.clickCard(context.millenniumFalcon);
+                expect(context.player1).toBeAbleToSelectExactly([context.pykeSentinel, context.gideonHask]);
+
+                context.player1.clickCard(context.pykeSentinel);
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.pykeSentinel).toBeCapturedBy(context.millenniumFalcon);
+
+                const battleDroids = context.player1.findCardsByName('battle-droid');
+                expect(battleDroids.length).toBe(2);
+                expect(battleDroids).toAllBeInZone('groundArena', context.player1);
+                expect(battleDroids.every((battleDroid) => battleDroid.exhausted)).toBeTrue();
+            });
+
+            it('should allow a friendly unit to capture an enemy non-leader, non-vehicle unit not generating 2 battle droid tokens because the captured unit does not cost less', () => {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.prisonerOfWar);
+                expect(context.player1).toBeAbleToSelectExactly([context.ezraBridger, context.millenniumFalcon]);
+
+                context.player1.clickCard(context.ezraBridger);
+                expect(context.player1).toBeAbleToSelectExactly([context.pykeSentinel, context.gideonHask]);
+
+                context.player1.clickCard(context.gideonHask);
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.gideonHask).toBeCapturedBy(context.ezraBridger);
+
+                const battleDroids = context.player1.findCardsByName('battle-droid');
+                expect(battleDroids.length).toBe(0);
+            });
+        });
+    });
+});
