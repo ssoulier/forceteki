a7146c574132cc407c4a69520658ad33d7f947e1
Stephane Soulier <stephane.soulier@alan.eu>
2025-01-05 15:53:08 +0100

    Sneak attack (#389)
    [Sneak Attack](https://swudb.com/card/SOR/219/sneak-attack)
    
    <img width="365" alt="Screenshot 2024-12-24 at 11 01 02"
    src="https://github.com/user-attachments/assets/cd6489e7-7816-4dc4-89d9-16dac6b9ac6e"
    />
    
    ---------
    
    Co-authored-by: Stephane Soulier <stephane.soulier@alan.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/01_SOR/events/SneakAttack.ts
@@ -0,0 +1,41 @@
+import { EventCard } from '../../../core/card/EventCard';
+import AbilityHelper from '../../../AbilityHelper';
+import { PhaseName, RelativePlayer, WildcardCardType, ZoneName } from '../../../core/Constants';
+import { CostAdjustType } from '../../../core/cost/CostAdjuster';
+
+export default class SneakAttack extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '3426168686',
+            internalName: 'sneak-attack'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Play a unit from your hand. It costs 3 less and enters play ready. At the start of the regroup phase, defeat it.',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                zoneFilter: ZoneName.Hand,
+                controller: RelativePlayer.Self,
+                immediateEffect: AbilityHelper.immediateEffects.playCardFromHand({
+                    entersReady: true,
+                    adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 3 }
+                }),
+            },
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'At the start of the regroup phase, defeat it.',
+                immediateEffect: AbilityHelper.immediateEffects.delayedCardEffect({
+                    title: 'Defeat it.',
+                    target: ifYouDoContext.events[0].card,
+                    when: {
+                        onPhaseStarted: (context) => context.phase === PhaseName.Regroup
+                    },
+                    immediateEffect: AbilityHelper.immediateEffects.defeat()
+                })
+            })
+        });
+    }
+}
+
+SneakAttack.implemented = true;
---
+++ test/server/cards/01_SOR/events/SneakAttack.spec.ts
@@ -0,0 +1,108 @@
+describe('Sneak Attack', function() {
+    integration(function(contextRef) {
+        describe('Sneak Attack\'s ability', function() {
+            it('should play Sabine for 1 resource less and defeat it at the end.', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['sneak-attack', 'sabine-wren#you-can-count-on-me', 'obiwan-kenobi#following-fate', 'recruit'],
+                        groundArena: ['battlefield-marine'],
+                        spaceArena: ['cartel-spacer'],
+                        base: 'administrators-tower',
+                        leader: 'luke-skywalker#faithful-friend',
+                        resources: 3
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.sneakAttack);
+                expect(context.player1).toBeAbleToSelectExactly([context.sabineWren]);
+                context.player1.clickCard(context.sabineWren);
+                expect(context.sabineWren.exhausted).toBeFalse();
+                expect(context.player1.readyResourceCount).toBe(0);
+
+                // Check that Sabine is defeated at the beginning of the regroup phase
+                context.moveToRegroupPhase();
+                expect(context.sabineWren).toBeInZone('discard');
+                expect(context.player1).toHavePrompt('Select between 0 and 1 cards to resource');
+            });
+
+            it('should not bug if there is no legal card to be played', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['sneak-attack', 'sabine-wren#you-can-count-on-me', 'recruit'],
+                        groundArena: ['battlefield-marine'],
+                        spaceArena: ['cartel-spacer'],
+                        base: 'administrators-tower',
+                        leader: 'luke-skywalker#faithful-friend',
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.sabineWren);
+                context.player2.passAction();
+                context.player1.clickCard(context.sneakAttack);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not bug if Sabine is defeated before the end of the phase', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['sneak-attack', 'sabine-wren#you-can-count-on-me', 'obiwan-kenobi#following-fate', 'recruit'],
+                        groundArena: ['battlefield-marine'],
+                        spaceArena: ['cartel-spacer'],
+                        base: 'administrators-tower',
+                        leader: 'luke-skywalker#faithful-friend',
+                        resources: 3
+                    },
+                    player2: {
+                        groundArena: ['atst'],
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.sneakAttack);
+                context.player1.clickCard(context.sabineWren);
+                context.player2.clickCard(context.atst);
+                context.player2.clickCard(context.sabineWren);
+                expect(context.sabineWren).toBeInZone('discard');
+            });
+
+            // TODO: Fix this test cf https://github.com/SWU-Karabast/forceteki/pull/389#discussion_r1898603793
+            // it('should not defeat Sabine if she is waylay back in hand and played back the same phase', function () {
+            //     contextRef.setupTest({
+            //         phase: 'action',
+            //         player1: {
+            //             hand: ['sneak-attack', 'sabine-wren#you-can-count-on-me', 'recruit'],
+            //             groundArena: ['battlefield-marine'],
+            //             spaceArena: ['cartel-spacer'],
+            //             base: 'administrators-tower',
+            //             leader: 'luke-skywalker#faithful-friend',
+            //         },
+            //         player2: {
+            //             hand: ['waylay']
+            //         }
+            //     });
+
+            //     const { context } = contextRef;
+
+            //     context.player1.clickCard(context.sneakAttack);
+            //     context.player1.clickCard(context.sabineWren);
+
+            //     // Waylay Sabine back in hand
+            //     context.player2.clickCard(context.waylay);
+            //     context.player2.clickCard(context.sabineWren);
+
+            //     // Sabine is played back
+            //     context.player1.clickCard(context.sabineWren);
+            //     context.nextPhase();
+            //     expect(context.sabineWren).toBeInZone('groundArena');
+            // });
+        });
+    });
+});
