686fb4139d298d163f5b6e94d76db178583c3558
Travis Chase <travis@travischase.me>
2025-01-21 14:24:32 -0600

    Implementing Kuiil, I Have Spoken (#449)
    ![image](https://github.com/user-attachments/assets/945a086d-49ad-4e9d-8e32-73f17d3d3ad5)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/units/KuiilIHaveSpoken.ts
@@ -0,0 +1,40 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class KuiilIHaveSpoken extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '5511838014',
+            internalName: 'kuiil#i-have-spoken',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addOnAttackAbility({
+            title: 'Discard a card from your deck.',
+            immediateEffect: AbilityHelper.immediateEffects.discardFromDeck((context) => ({
+                amount: 1,
+                target: context.source.controller
+            })),
+            ifYouDo: (context) => ({
+                title: 'If it shares an aspect with your base, return it to your hand.',
+                immediateEffect: AbilityHelper.immediateEffects.conditional({
+                    condition: this.hasMatchingAspects(context),
+                    onTrue: AbilityHelper.immediateEffects.returnToHand({
+                        target: context.events[0].card
+                    }),
+                    onFalse: AbilityHelper.immediateEffects.noAction()
+                })
+            })
+        });
+    }
+
+    private hasMatchingAspects(context) {
+        const baseAspects = context.source.controller.base.aspects;
+        const discardedCardAspects = context.events[0].card.aspects;
+
+        return baseAspects.some((aspect) => discardedCardAspects.includes(aspect));
+    }
+}
+
+KuiilIHaveSpoken.implemented = true;
---
+++ test/server/cards/02_SHD/units/KuiilIHaveSpoken.spec.ts
@@ -0,0 +1,60 @@
+
+describe('Kuiil, I Have Spoken', function () {
+    integration(function (contextRef) {
+        describe('Kuiil\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        leader: 'rey#more-than-a-scavenger',
+                        base: 'tarkintown',
+                        groundArena: ['kuiil#i-have-spoken'],
+                        deck: ['green-squadron-awing', 'restored-arc170'],
+                    }
+                });
+            });
+
+            it('should discard a card and only draw it if it shares an aspect with the base', function () {
+                const { context } = contextRef;
+
+                // Should draw the discarded card since it shares an aspect with the base
+                context.player1.clickCard(context.kuiil);
+                context.player1.clickCard(context.player2.base);
+
+                expect(context.player1).toHaveExactPromptButtons(['Discard a card from your deck.', 'Restore 1']);
+
+                context.player1.clickPrompt('Discard a card from your deck.');
+                expect(context.greenSquadronAwing).toBeInZone('hand');
+                expect(context.player2).toBeActivePlayer();
+
+                context.moveToNextActionPhase();
+
+                // Should not draw the discarded card since it does not share an aspect with the base
+                context.player1.setDeck([context.restoredArc170]);
+
+                context.player1.clickCard(context.kuiil);
+                context.player1.clickCard(context.player2.base);
+
+                expect(context.player1).toHaveExactPromptButtons(['Discard a card from your deck.', 'Restore 1']);
+
+                context.player1.clickPrompt('Discard a card from your deck.');
+                expect(context.restoredArc170).toBeInZone('discard');
+                expect(context.player2).toBeActivePlayer();
+
+                context.moveToNextActionPhase();
+
+                // Should do nothing since there are no cards in the deck
+                context.player1.setDeck([]);
+
+                context.player1.clickCard(context.kuiil);
+                context.player1.clickCard(context.player2.base);
+
+                expect(context.player1).toHaveExactPromptButtons(['Discard a card from your deck.', 'Restore 1']);
+
+                context.player1.clickPrompt('Discard a card from your deck.');
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
+
