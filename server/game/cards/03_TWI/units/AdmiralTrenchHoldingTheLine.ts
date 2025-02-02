9b057ebcc1159a0577a8673939291ac44f31c852
Travis Chase <travis@travischase.me>
2025-02-02 14:29:39 -0600

    Implementing Admiral Trench, Holding the Line (#468)
    ![image](https://github.com/user-attachments/assets/4f78fe73-1530-4101-9426-d4f9f3fe51df)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/03_TWI/units/AdmiralTrenchHoldingTheLine.ts
@@ -0,0 +1,38 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer, TargetMode, WildcardCardType, ZoneName } from '../../../core/Constants';
+import type { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import type { UnitsDefeatedThisPhaseWatcher } from '../../../stateWatchers/UnitsDefeatedThisPhaseWatcher';
+
+export default class AdmiralTrenchHoldingTheLine extends NonLeaderUnitCard {
+    private unitsDefeatedThisPhaseWatcher: UnitsDefeatedThisPhaseWatcher;
+
+    protected override getImplementationId () {
+        return {
+            id: '7013591351',
+            internalName: 'admiral-trench#holding-the-line'
+        };
+    }
+
+    protected override setupStateWatchers(registrar: StateWatcherRegistrar): void {
+        this.unitsDefeatedThisPhaseWatcher = AbilityHelper.stateWatchers.unitsDefeatedThisPhase(registrar, this);
+    }
+
+    public override setupCardAbilities () {
+        this.addWhenPlayedAbility({
+            title: 'Return up to 3 units that were defeated this phase from your discard pile to your hand.',
+            targetResolver: {
+                mode: TargetMode.UpTo,
+                numCards: 3,
+                cardTypeFilter: WildcardCardType.Unit,
+                zoneFilter: ZoneName.Discard,
+                controller: RelativePlayer.Self,
+                cardCondition: (card) => card.isUnit() && this.unitsDefeatedThisPhaseWatcher.wasDefeatedThisPhase(card),
+                immediateEffect: AbilityHelper.immediateEffects.returnToHand()
+            }
+        });
+    }
+}
+
+
+AdmiralTrenchHoldingTheLine.implemented = true;
---
+++ test/server/cards/03_TWI/units/AdmiralTrenchHoldingTheLine.spec.ts
@@ -0,0 +1,94 @@
+describe('Admiral Trench, Holding the Line', function () {
+    integration(function (contextRef) {
+        beforeEach(function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    hand: ['superlaser-blast', 'pillage'],
+                    groundArena: ['wampa'],
+                },
+                player2: {
+                    hand: ['admiral-trench#holding-the-line', 'the-emperors-legion', 'death-star-stormtrooper'],
+                    groundArena: ['first-legion-snowtrooper', 'maul#shadow-collective-visionary', { card: 'fifth-brother#fear-hunter', upgrades: ['fallen-lightsaber'] }],
+                    spaceArena: ['imperial-interceptor'],
+                    discard: ['open-fire', 'ruthless-raider']
+                }
+            });
+        });
+
+        it('should return up to 3 units defeated this phase to player hand from a discard pile', function () {
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.superlaserBlast);
+            expect(context.player2).toBeActivePlayer();
+
+            context.player2.clickCard(context.admiralTrenchHoldingTheLine);
+            expect(context.player2).toHavePrompt('Select 3 cards');
+            expect(context.player2).toBeAbleToSelectExactly([context.firstLegionSnowtrooper, context.maul, context.fifthBrother, context.imperialInterceptor]);
+            expect(context.player2).toHaveEnabledPromptButtons(['Done', 'Choose no target']);
+
+            context.player2.clickCard(context.firstLegionSnowtrooper);
+            context.player2.clickCard(context.maul);
+            context.player2.clickCard(context.fifthBrother);
+            context.player2.clickCardNonChecking(context.imperialInterceptor);
+            context.player2.clickPrompt('Done');
+
+            expect(context.player2.hand).toContain(context.firstLegionSnowtrooper);
+            expect(context.player2.hand).toContain(context.maul);
+            expect(context.player2.hand).toContain(context.fifthBrother);
+            expect(context.player2.hand).not.toContain(context.imperialInterceptor);
+            expect(context.imperialInterceptor).toBeInZone('discard', context.player2);
+        });
+
+        it('should return exploited unit since it was defeated this phase', function () {
+            const { context } = contextRef;
+
+            context.player1.passAction();
+
+            context.player2.clickCard(context.admiralTrenchHoldingTheLine);
+            context.player2.clickPrompt('Play Admiral Trench using Exploit');
+
+            expect(context.player2).toHavePrompt('Select 1 cards to exploit');
+
+            context.player2.clickCard(context.firstLegionSnowtrooper);
+            context.player2.clickPrompt('Done');
+
+            expect(context.player2).toHavePrompt('Select 3 cards');
+            expect(context.player2).toBeAbleToSelectExactly([context.firstLegionSnowtrooper]);
+            expect(context.player2).toHaveEnabledPromptButtons(['Done', 'Choose no target']);
+
+            context.player2.clickCard(context.firstLegionSnowtrooper);
+            context.player2.clickPrompt('Done');
+
+            expect(context.player2.hand).toContain(context.firstLegionSnowtrooper);
+        });
+
+        it('should not return that was defeated this phase if it become a new copy of a unit', function () {
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.wampa);
+            context.player1.clickCard(context.firstLegionSnowtrooper);
+
+            expect(context.firstLegionSnowtrooper).toBeInZone('discard', context.player2);
+
+            context.player2.clickCard(context.theEmperorsLegion);
+
+            expect(context.player2.hand).toContain(context.firstLegionSnowtrooper);
+
+            context.player1.clickCard(context.pillage);
+            context.player1.clickPrompt('Opponent');
+            context.player2.clickCard(context.deathStarStormtrooper);
+            context.player2.clickCard(context.firstLegionSnowtrooper);
+            context.player2.clickPrompt('Done');
+
+            expect(context.deathStarStormtrooper).toBeInZone('discard', context.player2);
+            expect(context.firstLegionSnowtrooper).toBeInZone('discard', context.player2);
+
+            context.player2.clickCard(context.admiralTrenchHoldingTheLine);
+            context.player2.clickPrompt('Play Admiral Trench');
+
+            // Should not be able to return any cards since First Legion Snowtrooper is a new copy and was not defeated this phase
+            expect(context.player1).toBeActivePlayer();
+        });
+    });
+});
