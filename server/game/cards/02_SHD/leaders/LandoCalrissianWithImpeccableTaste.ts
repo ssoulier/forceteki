432eac74a5177f53054c94d7a22c1c4fa8ba777c
Kevin Ray <kevin.l.ray@gmail.com>
2025-02-03 11:05:27 -0600

    Implement Lando Calrissian, With Impeccable Taste (#515)
    ![image](https://github.com/user-attachments/assets/a05a6ce7-0dd1-4c72-8200-2d971e8ada98)
    
    ![image](https://github.com/user-attachments/assets/bf4c4ab2-5e86-42e9-b746-5cb053697662)
    Looks like order of defeating resource and hot shot attack is correct
    based off of this:
    
    ![image](https://github.com/user-attachments/assets/ba9f02b9-0a8d-4cee-abb5-5f87f901eb42)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/02_SHD/leaders/LandoCalrissianWithImpeccableTaste.ts
@@ -0,0 +1,59 @@
+import AbilityHelper from '../../../AbilityHelper';
+import type { AbilityContext } from '../../../core/ability/AbilityContext';
+import type { TriggeredAbilityContext } from '../../../core/ability/TriggeredAbilityContext';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+import { PlayType, RelativePlayer, ZoneName } from '../../../core/Constants';
+import { CostAdjustType } from '../../../core/cost/CostAdjuster';
+import type { IThenAbilityPropsWithSystems } from '../../../Interfaces';
+import type { ICardTargetResolver } from '../../../TargetInterfaces';
+
+export default class LandoCalrissianWithImpeccableTaste extends LeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '5440730550',
+            internalName: 'lando-calrissian#with-impeccable-taste',
+        };
+    }
+
+    private buildSmuggleCardAbility(): ICardTargetResolver<TriggeredAbilityContext<this>> {
+        return {
+            controller: RelativePlayer.Self,
+            zoneFilter: ZoneName.Resource,
+            immediateEffect: AbilityHelper.immediateEffects.playCard({
+                playType: PlayType.Smuggle,
+                adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 2 }
+            })
+        };
+    }
+
+    private buildDefeatResourceAbility(): IThenAbilityPropsWithSystems<AbilityContext> {
+        return {
+            title: 'Defeat a resource you own and control',
+            targetResolver: {
+                controller: RelativePlayer.Self,
+                zoneFilter: ZoneName.Resource,
+                cardCondition: (card) => card.owner === this.controller,
+                activePromptTitle: 'Defeat a resource you own and control',
+                immediateEffect: AbilityHelper.immediateEffects.defeat()
+            }
+        };
+    }
+
+    protected override setupLeaderSideAbilities() {
+        this.addActionAbility({
+            title: 'Play a card using Smuggle. It costs 2 less. Defeat a resource you own and control.',
+            cost: AbilityHelper.costs.exhaustSelf(),
+            targetResolver: this.buildSmuggleCardAbility(),
+            then: this.buildDefeatResourceAbility()
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities() {
+        this.addActionAbility({
+            title: 'Play a card using Smuggle. It costs 2 less. Defeat a resource you own and control. Use this ability only once each round',
+            limit: AbilityHelper.limit.perRound(1),
+            targetResolver: this.buildSmuggleCardAbility(),
+            then: this.buildDefeatResourceAbility()
+        });
+    }
+}
---
+++ test/server/cards/02_SHD/leaders/LandoCalrissianWithImpeccableTaste.spec.ts
@@ -0,0 +1,113 @@
+describe('Lando Calrissian With Impeccable Taste', function () {
+    integration(function (contextRef) {
+        it('Lando Calrissian\'s undeployed ability plays card and defeats resource', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    leader: 'lando-calrissian#with-impeccable-taste',
+                    hand: ['pyke-sentinel', 'wampa'],
+                    resources: ['zorii-bliss#valiant-smuggler', 'aggression', 'spark-of-rebellion', 'protector', 'hotshot-dl44-blaster'],
+                    deck: ['liberated-slaves'],
+                    groundArena: ['boba-fett#disintegrator']
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Activate Lando ability
+            context.player1.clickCard(context.landoCalrissian);
+            context.player1.clickPrompt('Play a card using Smuggle. It costs 2 less. Defeat a resource you own and control.');
+            expect(context.player1).toBeAbleToSelectExactly([context.zoriiBliss, context.hotshotDl44Blaster]);
+
+            // Play hot shot blaster
+            context.player1.clickCard(context.hotshotDl44Blaster);
+            context.player1.clickCard(context.bobaFett);
+
+            expect(context.landoCalrissian.exhausted).toBe(true);
+            expect(context.liberatedSlaves).toBeInZone('resource');
+
+            // Proceeds to defeat resource
+            expect(context.player1).toHavePrompt('Defeat a resource you own and control');
+            expect(context.player1).toBeAbleToSelectExactly([context.aggression, context.protector, context.sparkOfRebellion, context.zoriiBliss, context.liberatedSlaves]);
+
+            // Defeat a resource
+            context.player1.clickCard(context.aggression);
+            expect(context.player1.readyResourceCount).toBe(4);
+            expect(context.aggression).toBeInZone('discard');
+
+            // Blaster attack is after resource was defeated
+            context.player1.clickCard(context.p2Base);
+        });
+
+        it('Lando Calrissian\'s deployed ability plays card and defeats resource', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    leader: { card: 'lando-calrissian#with-impeccable-taste', deployed: true },
+                    hand: ['pyke-sentinel', 'wampa'],
+                    resources: ['zorii-bliss#valiant-smuggler', 'aggression', 'spark-of-rebellion', 'protector', 'dj#blatant-thief'],
+                    deck: ['liberated-slaves'],
+                    groundArena: ['boba-fett#disintegrator'],
+                    base: 'administrators-tower'
+                },
+                player2: {
+                    resources: ['resupply'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Activate Lando ability
+            context.player1.clickCard(context.landoCalrissian);
+            context.player1.clickPrompt('Play a card using Smuggle. It costs 2 less. Defeat a resource you own and control. Use this ability only once each round');
+            expect(context.player1).toBeAbleToSelectExactly([context.zoriiBliss, context.dj]);
+
+            // Play DJ
+            context.player1.clickCard(context.dj);
+            expect(context.liberatedSlaves).toBeInZone('resource');
+
+            // Stolen resource is not available to defeat
+            expect(context.player1).toHavePrompt('Defeat a resource you own and control');
+            expect(context.player1).toBeAbleToSelectExactly([context.aggression, context.protector, context.sparkOfRebellion, context.zoriiBliss, context.liberatedSlaves]);
+
+            // Defeat card that was resourced after smuggle
+            context.player1.clickCard(context.liberatedSlaves);
+            expect(context.liberatedSlaves).toBeInZone('discard');
+            expect(context.player1.readyResourceCount).toBe(1);
+        });
+
+        it('Lando Calrissian\'s deployed ability as a soft pass', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    leader: { card: 'lando-calrissian#with-impeccable-taste', deployed: true },
+                    hand: ['pyke-sentinel', 'wampa'],
+                    resources: ['zorii-bliss#valiant-smuggler', 'aggression', 'spark-of-rebellion', 'protector', 'dj#blatant-thief'],
+                    deck: ['liberated-slaves'],
+                    groundArena: ['boba-fett#disintegrator'],
+                    base: 'administrators-tower'
+                },
+                player2: {
+                    resources: ['resupply'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Activate Lando ability
+            context.player1.clickCard(context.landoCalrissian);
+            context.player1.clickPrompt('Play a card using Smuggle. It costs 2 less. Defeat a resource you own and control. Use this ability only once each round');
+            expect(context.player1).toBeAbleToSelectExactly([context.zoriiBliss, context.dj]);
+
+            // Choose to not play a card
+            context.player1.clickPrompt('Choose no target');
+
+            // Still requires to defeat a resource
+            expect(context.player1).toHavePrompt('Defeat a resource you own and control');
+            expect(context.player1).toBeAbleToSelectExactly([context.aggression, context.protector, context.sparkOfRebellion, context.zoriiBliss, context.dj]);
+            context.player1.clickCard(context.protector);
+            expect(context.protector).toBeInZone('discard');
+            expect(context.player1.readyResourceCount).toBe(4);
+        });
+    });
+});
