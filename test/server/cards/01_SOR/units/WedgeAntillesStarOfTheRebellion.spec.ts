992070ac44db4767c04840c49887f8872ee591c0
Robbie Moyer <moyerr@users.noreply.github.com>
2025-03-04 11:39:39 -0500

    Add test case for Wedge Antilles (SOR) interaction with token vehicles (#759)

--- test/server/cards/01_SOR/units/WedgeAntillesStarOfTheRebellion.spec.ts
+++ test/server/cards/01_SOR/units/WedgeAntillesStarOfTheRebellion.spec.ts
@@ -5,47 +5,71 @@ describe('Wedge Antilles, Star of the Rebellion', function() {
                 return contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
-                        hand: ['alliance-xwing', 'battlefield-marine'],
+                        hand: ['alliance-xwing', 'battlefield-marine', 'veteran-fleet-officer'],
                         groundArena: ['wedge-antilles#star-of-the-rebellion'],
                     },
                     player2: {
                         spaceArena: ['hwk290-freighter'],
                         hand: ['atst']
-                    },
-
-                    // IMPORTANT: this is here for backwards compatibility of older tests, don't use in new code
-                    autoSingleTarget: true
+                    }
                 });
             });
 
             it('should give Ambush and +1/+1 to a friendly Vehicle unit', function () {
                 const { context } = contextRef;
 
-                // Case 1: It should give Ambush and +1/+1 to a friendly Vehicle unit.
                 context.player1.clickCard(context.allianceXwing);
                 expect(context.player1).toHaveExactPromptButtons(['Trigger', 'Pass']);
                 context.player1.clickPrompt('Trigger');
+
+                context.player1.clickCard(context.hwk290Freighter);
 
                 expect(context.allianceXwing.exhausted).toBeTrue();
                 expect(context.allianceXwing.getPower()).toBe(3);
                 expect(context.allianceXwing.getHp()).toBe(4);
                 expect(context.allianceXwing.damage).toBe(2);
                 expect(context.hwk290Freighter.damage).toBe(3);
+            });
 
-                // Case 2: It should not give Ambush and +1/+1 to an enemy Vehicle unit
+            it('should not give Ambush and +1/+1 to an enemy Vehicle unit', function () {
+                const { context } = contextRef;
+
+                context.player1.passAction();
                 context.player2.clickCard(context.atst);
 
                 expect(context.atst).toBeInZone('groundArena');
                 expect(context.atst.getPower()).toBe(6);
                 expect(context.atst.getHp()).toBe(7);
                 expect(context.player1).toBeActivePlayer();
+            });
 
-                // Case 3: It should not give Ambush and +1/+1 to a friendly non-Vehicle unit
+            it('should not give Ambush and +1/+1 to a friendly non-Vehicle unit', function () {
+                const { context } = contextRef;
+
                 context.player1.clickCard(context.battlefieldMarine);
                 expect(context.battlefieldMarine).toBeInZone('groundArena');
                 expect(context.battlefieldMarine.getPower()).toBe(3);
                 expect(context.battlefieldMarine.getHp()).toBe(3);
                 expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should give Ambush and +1/+1 to friendly token Vehicle units', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.veteranFleetOfficer);
+
+                expect(context.player1).toHaveExactPromptButtons(['Trigger', 'Pass']);
+                context.player1.clickPrompt('Trigger');
+
+                context.player1.clickCard(context.hwk290Freighter);
+
+                const xwing = context.player1.findCardByName('xwing');
+
+                expect(xwing.exhausted).toBeTrue();
+                expect(xwing.getPower()).toBe(3);
+                expect(xwing.getHp()).toBe(3);
+                expect(xwing.damage).toBe(2);
+                expect(context.hwk290Freighter.damage).toBe(3);
             });
         });
     });
