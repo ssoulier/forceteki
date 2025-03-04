0d84231b76349850405d1f05a566b7b5b6e2fc5a
Stephane Soulier <stephane.soulier@alan.eu>
2025-03-04 14:56:18 +0100

    Shuttle st149 (#747)
    [card](https://swudb.com/card/JTL/242/shuttle-st-149-under-krennic's-authority)
    
    <img width="329" alt="Screenshot 2025-03-03 at 20 48 52"
    src="https://github.com/user-attachments/assets/bedb60a1-49c5-45d5-9b5d-cc098d9807c3"
    />
    
    ---------
    
    Co-authored-by: Stephane Soulier <stephane.soulier@alan.com>
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/04_JTL/units/ShuttleST149UnderKrennicsAuthority.ts
@@ -0,0 +1,45 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer, WildcardCardType, WildcardRelativePlayer, WildcardZoneName } from '../../../core/Constants';
+
+export default class ShuttleST149UnderKrennicsAuthority extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '1519837763',
+            internalName: 'shuttle-st149#under-krennics-authority',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Take control of a token upgrade on a unit and attach it to a different eligible unit.',
+            optional: true,
+            when: {
+                onCardPlayed: (event, context) => event.card === context.source,
+                onCardDefeated: (event, context) => event.card === context.source,
+            },
+            targetResolvers: {
+                chooseUpgrade: {
+                    cardTypeFilter: WildcardCardType.Upgrade,
+                    zoneFilter: WildcardZoneName.AnyArena,
+                    controller: WildcardRelativePlayer.Any,
+                    cardCondition: (card) => card.isToken(),
+                },
+                chooseUnit: {
+                    dependsOn: 'chooseUpgrade',
+                    cardTypeFilter: WildcardCardType.Unit,
+                    zoneFilter: WildcardZoneName.AnyArena,
+                    controller: WildcardRelativePlayer.Any,
+                    cardCondition: (card, context) =>
+                        context.targets.chooseUpgrade.parentCard !== card &&
+                        context.targets.chooseUpgrade.canAttach(card, context.targets.chooseUpgrade.parentCard.controller),
+                    immediateEffect: AbilityHelper.immediateEffects.attachUpgrade((context) => ({
+                        upgrade: context.targets.chooseUpgrade,
+                        target: context.targets.chooseUnit,
+                        newController: RelativePlayer.Self,
+                    })),
+                }
+            }
+        });
+    }
+}
---
+++ test/server/cards/04_JTL/units/ShuttleST149UnderKrennicsAuthority.spec.ts
@@ -0,0 +1,145 @@
+describe('Shuttle ST-149 Under Krennics Authority', function() {
+    integration(function(contextRef) {
+        describe('Shuttle ST-149\'s ability', function() {
+            beforeEach(function () {
+                return contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['shuttle-st149#under-krennics-authority', 'entrenched'],
+                        groundArena: ['fugitive-wookiee', 'battlefield-marine', { card: 'atst', exhausted: true, upgrades: ['frozen-in-carbonite'] }],
+                        spaceArena: [{ card: 'avenger#hunting-star-destroyer', upgrades: ['shield'] }],
+                        leader: { card: 'finn#this-is-a-rescue', deployed: true },
+                    },
+                    player2: {
+                        hand: ['electrostaff', 'vanquish'],
+                        groundArena: ['wampa', { card: 'hylobon-enforcer', upgrades: ['legal-authority', 'experience'] }],
+                        spaceArena: ['cartel-spacer'],
+                        leader: { card: 'iden-versio#inferno-squad-commander', deployed: true },
+                    }
+                });
+            });
+
+            it('should allow, when played, to move a token upgrade to another eligible unit with a different controller', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.shuttleSt149);
+                context.player1.clickPrompt('Take control of a token upgrade on a unit and attach it to a different eligible unit.');
+                expect(context.player1).toBeAbleToSelectExactly([context.experience, context.shield]);
+                expect(context.player1).toHavePassAbilityButton();
+
+                context.player1.clickCard(context.experience);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.fugitiveWookiee,
+                    context.battlefieldMarine,
+                    context.atst,
+                    context.avenger,
+                    context.idenVersio,
+                    context.shuttleSt149,
+                    context.wampa,
+                    context.cartelSpacer,
+                    context.finn
+                ]);
+
+                context.player1.clickCard(context.fugitiveWookiee);
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.fugitiveWookiee).toHaveExactUpgradeNames(['experience']);
+
+                // Shielded should resolve now
+                expect(context.shuttleSt149).toHaveExactUpgradeNames(['shield']);
+
+                // We test the controller with of the shield with Finn
+                context.player2.passAction();
+                context.player1.clickCard(context.finn);
+                context.player1.clickCard(context.p2Base);
+                const avengerShield = context.avenger.upgrades[0];
+                const shuttleShield = context.shuttleSt149.upgrades[0];
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.experience,
+                    avengerShield,
+                    shuttleShield,
+                    context.frozenInCarbonite
+                ]);
+                context.player1.clickCard(context.experience);
+                expect(context.fugitiveWookiee).toHaveExactUpgradeNames(['shield']);
+            });
+
+            it('should allow, when defeated, to move a token upgrade to another eligible unit with a different controller', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.shuttleSt149);
+                context.player1.clickPrompt('Take control of a token upgrade on a unit and attach it to a different eligible unit.');
+                expect(context.player1).toBeAbleToSelectExactly([context.experience, context.shield]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickPrompt('Pass');
+
+                // Shielded should resolve now
+                expect(context.shuttleSt149).toHaveExactUpgradeNames(['shield']);
+
+                expect(context.player2).toBeActivePlayer();
+
+                // Defeat the Shuttle
+                context.player2.clickCard(context.vanquish);
+                context.player2.clickCard(context.shuttleSt149);
+                context.player2.clickPrompt('You');
+                expect(context.shuttleSt149).toBeInZone('discard');
+                const avengerShield = context.avenger.upgrades[0];
+                expect(context.player1).toBeAbleToSelectExactly([context.experience, avengerShield]);
+                expect(context.player1).toHavePassAbilityButton();
+
+                context.player1.clickCard(avengerShield);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.fugitiveWookiee,
+                    context.battlefieldMarine,
+                    context.atst,
+                    context.idenVersio,
+                    context.wampa,
+                    context.hylobonEnforcer,
+                    context.cartelSpacer,
+                    context.finn
+                ]);
+
+                context.player1.clickCard(context.idenVersio);
+                expect(context.idenVersio).toHaveExactUpgradeNames(['shield']);
+
+                // The shild on idenVersion should remain controlled by player1
+                // So Finn should be able to defeat it
+                context.player1.clickCard(context.finn);
+                context.player1.clickCard(context.p2Base);
+                const idenShield = context.idenVersio.upgrades[0];
+                expect(context.player1).toBeAbleToSelectExactly([idenShield, context.frozenInCarbonite]);
+                context.player1.clickCard(idenShield);
+                expect(context.idenVersio).toHaveExactUpgradeNames(['shield']);
+            });
+
+            it('should allow, when played, to move the shield token from shielded', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.shuttleSt149);
+                context.player1.clickPrompt('Shielded');
+                expect(context.shuttleSt149).toHaveExactUpgradeNames(['shield']);
+                const shuttleShield = context.shuttleSt149.upgrades[0];
+                const avengerShield = context.avenger.upgrades[0];
+                expect(context.player1).toBeAbleToSelectExactly([context.experience, shuttleShield, avengerShield]);
+                expect(context.player1).toHavePassAbilityButton();
+
+                context.player1.clickCard(shuttleShield);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.fugitiveWookiee,
+                    context.battlefieldMarine,
+                    context.atst,
+                    context.avenger,
+                    context.hylobonEnforcer,
+                    context.idenVersio,
+                    context.wampa,
+                    context.cartelSpacer,
+                    context.finn
+                ]);
+
+                context.player1.clickCard(context.atst);
+                expect(context.atst).toHaveExactUpgradeNames(['shield', 'frozen-in-carbonite']);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
