c27db4ac4bf45e9ab07d9aac632bc6692a3d064c
brianries <briantries@gmail.com>
2025-03-21 08:20:38 -0500

    Logger switch for CloudWatch newlines (#978)
    Added a switch to the logger to do literal newlines for CloudWatch when
    running under the AWS environment. This will collapse a stack trace into
    a singular entries with explicit new line characters (hopefully
    rendering correctly when selected via LogInsight)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

--- server/logger.ts
+++ server/logger.ts
@@ -1,5 +1,8 @@
 import * as winston from 'winston';
 
+
+const isRunningInAWS = !!process.env.AWS_EXECUTION_ENV;
+
 const logFormatter = winston.format.printf(({ timestamp, level, message, stack }) => {
     const log = `${timestamp} [${level}]: ${message}`;
     return stack ? log + '\n' + stack : log;
@@ -10,6 +13,6 @@ export const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.errors({ stack: true }),
-        logFormatter
+        isRunningInAWS ? winston.format.json() : logFormatter
     )
 });
