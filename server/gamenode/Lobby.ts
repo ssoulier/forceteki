e5e247fd0ad2e31d4624b98eab67813976d672a3
AMMayberry1 <ammayberry1@gmail.com>
2025-03-24 23:36:50 -0400

    Add try / catch to onGameMessage and onLobbyMessage (#1001)

--- server/gamenode/Lobby.ts
+++ server/gamenode/Lobby.ts
@@ -525,33 +525,41 @@ export class Lobby {
     }
 
     private async onLobbyMessage(socket: Socket, command: string, ...args): Promise<void> {
-        if (!this[command] || typeof this[command] !== 'function') {
-            throw new Error(`Incorrect command or command format expected function but got: ${command}`);
-        }
+        try {
+            if (!this[command] || typeof this[command] !== 'function') {
+                throw new Error(`Incorrect command or command format expected function but got: ${command}`);
+            }
 
-        await this[command](socket, ...args);
-        this.sendLobbyState();
+            await this[command](socket, ...args);
+            this.sendLobbyState();
+        } catch (error) {
+            logger.error('Error processing lobby message', { error: { message: error.message, stack: error.stack }, lobbyId: this.id });
+        }
     }
 
     private async onGameMessage(socket: Socket, command: string, ...args): Promise<void> {
-        if (!this.game) {
-            return;
+        try {
+            if (!this.game) {
+                return;
+            }
+
+            // if (command === 'leavegame') {
+            //     return this.onLeaveGame(socket);
+            // }
+
+            if (!this.game[command] || typeof this.game[command] !== 'function') {
+                return;
+            }
+
+            this.game.stopNonChessClocks();
+            await this.game[command](socket.user.id, ...args);
+
+            this.game.continue();
+
+            this.sendGameState(this.game);
+        } catch (error) {
+            logger.error('Error processing game message', { error: { message: error.message, stack: error.stack }, lobbyId: this.id });
         }
-
-        // if (command === 'leavegame') {
-        //     return this.onLeaveGame(socket);
-        // }
-
-        if (!this.game[command] || typeof this.game[command] !== 'function') {
-            return;
-        }
-
-        this.game.stopNonChessClocks();
-        await this.game[command](socket.user.id, ...args);
-
-        this.game.continue();
-
-        this.sendGameState(this.game);
     }
 
 
